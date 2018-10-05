1b2d1079f947174a3a8100e0905ce096197cfedb
Conner Alegre <cga0@pm.me>
2018-10-05 09:02:25 -0600

    initial commit

---
+++ ios-lyric-finder/ios-lyric-finder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ios-lyric-finder
+//
+//  Created by Conner on 10/5/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end

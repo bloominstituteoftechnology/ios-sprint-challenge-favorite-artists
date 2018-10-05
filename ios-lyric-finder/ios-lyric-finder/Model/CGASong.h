9406760989e4e5e114270df342312e2ae124d5a2
Conner Alegre <cga0@pm.me>
2018-10-05 09:32:53 -0600

    implement song model

---
+++ ios-lyric-finder/ios-lyric-finder/Model/CGASong.h
@@ -0,0 +1,24 @@
+//
+//  CGASong.h
+//  ios-lyric-finder
+//
+//  Created by Conner on 10/5/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGASong : NSObject
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist lyrics:(NSString *)lyrics rating:(int) rating;
+
+@property NSString *title;
+@property NSString *artist;
+@property NSString *lyrics;
+@property int rating;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-lyric-finder/ios-lyric-finder/Model/CGASong.m
@@ -0,0 +1,13 @@
+//
+//  CGASong.m
+//  ios-lyric-finder
+//
+//  Created by Conner on 10/5/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+#import "CGASong.h"
+
+@implementation CGASong
+
+@end

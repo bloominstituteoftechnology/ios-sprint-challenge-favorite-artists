15ec06b8a01795b79f14b3d3ed89e9d8babe8016
Linh Bouniol <paulalinh89@gmail.com>
2018-10-05 08:09:13 -0700

    Initial commit

---
+++ LyricFinder/LyricFinder/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LyricFinder
+//
+//  Created by Linh Bouniol on 10/5/18.
+//  Copyright © 2018 Linh Bouniol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

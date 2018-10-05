c0333c0e3f8a3d9b337f89f0efd9c6f8e6601cfc
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-05 08:26:12 -0700

    new project

---
+++ Lyric Finder/Lyric Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lyric Finder
+//
+//  Created by Jonathan T. Miles on 10/5/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lyric Finder/Lyric Finder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Lyric Finder
+//
+//  Created by Jonathan T. Miles on 10/5/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end

06a77486d21658abea01acfa3b5f6cd187ee613d
Andrew Liao <andrewoliao@gmail.com>
2018-10-05 17:19:57 -0400

    Implement search function on detail view controller

--- Lyric Finder/Lyric Finder/View Controllers/AELDetailViewController.m
+++ Lyric Finder/Lyric Finder/View Controllers/AELDetailViewController.m
@@ -29,23 +29,52 @@
     }
     if([self song]){
         //if there is a song
+        [self setTitle: @"View Song Lyric"];
+        
         [[self songTitleField] setText: [[self song] title]];
         [[self artistField] setText: [[self song] artist]];
         [[self lyricTextView] setText: [[self song] lyrics]];
-        
+        [[self ratingLabel] setText: [@(self.song.rating) stringValue]];
         
     } else {
+        [self setTitle: @"Add Song Lyric"];
+        
+        [[self lyricTextView] setText: @""];
+        [[self ratingLabel] setText: @"0"];
         
     }
 }
 
 
 - (IBAction)save:(id)sender {
+    NSString *title = [[self songTitleField] text];
+    NSString *artist = [[self artistField] text];
+    NSString *lyrics = [[self lyricTextView] text];
+    NSUInteger rating = [[[self ratingLabel] text] intValue];
+    
+    if([self song]){
+        [[self songController] addSongWithTitle:title artist:artist lyrics:lyrics rating:rating ];
+    } else {
+        [[self songController] updateSong:[self song] lyrics:lyrics rating:rating];
+    }
 }
 
 - (IBAction)ratingControl:(id)sender {
 }
 
 - (IBAction)search:(id)sender {
+    NSString *title = [[self songTitleField] text];
+    NSString *artist = [[self artistField] text];
+    
+    [[self songController] searchForLyricsWithTitle:title by:artist completion:^(NSString * lyrics, NSError * error) {
+        if (error){
+            NSLog(@"Error searching for lyrics: %@", error);
+            return;
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[self lyricTextView] setText: lyrics];
+        });
+        
+    }];
 }
 @end

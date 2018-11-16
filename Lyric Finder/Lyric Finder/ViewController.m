346f31872f47c20cdc3d2de7b81e5df2ac444668
Scott Bennett <stt.btt@gmail.com>
2018-11-16 10:55:17 -0500

    Initial Commit

---
+++ Lyric Finder/Lyric Finder/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Lyric Finder
+//
+//  Created by Scott Bennett on 11/16/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end

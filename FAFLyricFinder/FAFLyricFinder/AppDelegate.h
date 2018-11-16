8cb03c932a284847e59e74f9f84ec38652cb3dbd
Farhan Farooqui <farhan@farooqui.net>
2018-11-16 11:43:52 -0500

    Project Setup

---
+++ FAFLyricFinder/FAFLyricFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FAFLyricFinder
+//
+//  Created by Farhan on 11/16/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FAFLyricFinder/FAFLyricFinder/FAFSongsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FAFSongsTableViewController.h
+//  FAFLyricFinder
+//
+//  Created by Farhan on 11/16/18.
+//  Copyright © 2018 Farhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FAFSongsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

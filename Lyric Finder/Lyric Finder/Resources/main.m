a7b26d0d12ea56fcce6a604a0c0779de4276ee89
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-11-16 10:02:47 -0600

    Storyboard completed

---
+++ Lyric Finder/Lyric Finder/Resources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lyric Finder
+//
+//  Created by Ilgar Ilyasov on 11/16/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

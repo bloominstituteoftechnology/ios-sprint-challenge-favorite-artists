2be451071c4a4989e2e36c0860c76ba3f33b33de
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-01-25 09:30:07 -0800

    initial Commit / Storyboard finished

---
+++ JWLyricFinder/JWLyricFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWLyricFinder
+//
+//  Created by Jerrick Warren on 1/25/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWLyricFinder/JWLyricFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JWLyricFinder
+//
+//  Created by Jerrick Warren on 1/25/19.
+//  Copyright © 2019 Jerrick Warren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

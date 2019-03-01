1f1f47e8ee1556831685ca8263b20c51687d7566
IvanCaldwell <grumpy.caldwell@gmail.com>
2019-03-01 09:12:11 -0800

    Setup Storyboard

---
+++ Lyric Finder/Lyric Finder/View Controllers/LyricFinderTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  LyricFinderTableViewController.h
+//  Lyric Finder
+//
+//  Created by Ivan Caldwell on 3/1/19.
+//  Copyright © 2019 Ivan Caldwell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LyricFinderTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

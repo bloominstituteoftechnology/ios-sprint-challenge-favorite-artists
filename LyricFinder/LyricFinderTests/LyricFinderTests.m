426853680b69a04b23f9d643a602bef03586e5d9
Dillon McElhinney <dillon.mce@gmail.com>
2019-03-01 09:04:33 -0700

    Initial Commit
    Layout views in storyboard

---
+++ LyricFinder/LyricFinderTests/LyricFinderTests.m
@@ -0,0 +1,37 @@
+//
+//  LyricFinderTests.m
+//  LyricFinderTests
+//
+//  Created by Dillon McElhinney on 3/1/19.
+//  Copyright © 2019 Dillon McElhinney. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LyricFinderTests : XCTestCase
+
+@end
+
+@implementation LyricFinderTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

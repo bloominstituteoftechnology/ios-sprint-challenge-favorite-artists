fe22f1631c8b73d4a948ce23ab0c15c21f502661
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2019-03-01 10:23:50 -0600

    Storyboard setup completed

---
+++ Sprint Challenge 11 - Lyric Finder/Sprint Challenge 11 - Lyric Finder/View Controllers/SongDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongDetailViewController.h
+//  
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SongDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Sprint Challenge 11 - Lyric Finder/Sprint Challenge 11 - Lyric Finder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sprint Challenge 11 - Lyric Finder
+//
+//  Created by Audrey Welch on 3/1/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

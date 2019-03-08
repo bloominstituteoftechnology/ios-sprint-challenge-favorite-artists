54294cb84adbf28bf2923e41fca7b110663155d7
Lotanna <sugabelly@gmail.com>
2019-03-08 12:03:09 -0500

    created lyric model
    Also got API key

--- Lyric Finder/Lyric Finder/LyricModel.h
+++ Lyric Finder/Lyric Finder/LyricModel.h
@@ -6,4 +6,19 @@
 //  Copyright © 2019 Sugabelly LLC. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LyricModel : NSObject
+
+@property (copy) NSString *song;
+@property (copy) NSString *musician;
+@property (copy) NSString *lyrics;
+@property int rating;
+
+- (instancetype)init: (NSString *)song artist: (NSString *)musician lyrics: (NSString *)lyrics score: (int)rating;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Lyric Finder/Lyric Finder/LyricModel.m
+++ Lyric Finder/Lyric Finder/LyricModel.m
@@ -6,3 +6,19 @@
 //  Copyright © 2019 Sugabelly LLC. All rights reserved.
 //
 
+#import "LyricModel.h"
+
+@implementation LyricModel
+
+- (instancetype)init:(NSString *)song artist:(NSString *)musician lyrics:(NSString *)lyrics score:(int)rating {
+    self = [super init];
+    if (self != nil) {
+        self.musician = musician;
+        self.song = song;
+        self.lyrics = lyrics;
+        self.rating = rating;
+    }
+    return self;
+}
+
+@end

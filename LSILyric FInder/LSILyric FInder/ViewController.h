63851041c70bfd2ec526c317cb87ee9e047d0526
iyinraphael <iyinraphael@gmail.com>
2019-03-08 07:32:35 -0800

    initial commit

---
+++ LSILyric FInder/LSILyric FInder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSILyric FInder
+//
+//  Created by Iyin Raphael on 3/8/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LSILyric FInder/LSILyric FInder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LSILyric FInder
+//
+//  Created by Iyin Raphael on 3/8/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

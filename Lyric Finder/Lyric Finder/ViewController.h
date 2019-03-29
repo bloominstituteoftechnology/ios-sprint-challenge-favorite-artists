f83b42e8f6de8eaeec69a7ca770ea277d50c1a38
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-29 09:49:27 -0700

    initial commit

---
+++ Lyric Finder/Lyric Finder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lyric Finder
+//
+//  Created by Moses Robinson on 3/29/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

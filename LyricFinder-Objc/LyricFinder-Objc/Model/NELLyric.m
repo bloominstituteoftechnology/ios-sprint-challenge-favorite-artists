9e076b7fba25169df40034135d72d88a8f8876a4
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-29 07:40:57 -0400

    Add model implementation

--- LyricFinder-Objc/LyricFinder-Objc/Model/NELLyric.m
+++ LyricFinder-Objc/LyricFinder-Objc/Model/NELLyric.m
@@ -10,4 +10,26 @@
 
 @implementation NELLyric
 
+- (instancetype)intiWithDictionary:(NSDictionary *)dictionary
+{
+    NSString *title = [dictionary objectForKey:@"title"];
+    NSString *artist = [dictionary objectForKey:@"artist"];
+    NSString *lyrics = [dictionary objectForKey:@"lyrics"];
+    NSNumber *rating = [dictionary objectForKey:@"rating"];
+    
+    return [self initWithTitle:title artist:artist lyrics:lyrics rating:[rating intValue]];
+}
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist lyrics:(NSString *)lyrics rating:(int)rating
+{
+    self = [super init];
+    if (self) {
+        _title = title;
+        _artist = artist;
+        _lyrics = lyrics;
+        _rating = rating;
+    }
+    return self;
+}
+
 @end

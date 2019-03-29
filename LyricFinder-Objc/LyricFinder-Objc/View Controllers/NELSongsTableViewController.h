0f4c3321fc7e93fda3d4cfd95cc3f68c3efa63dc
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-29 08:35:00 -0400

    Implement tableVC

--- LyricFinder-Objc/LyricFinder-Objc/View Controllers/NELSongsTableViewController.h
+++ LyricFinder-Objc/LyricFinder-Objc/View Controllers/NELSongsTableViewController.h
@@ -9,8 +9,11 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@class NELLyricController;
 
 @interface NELSongsTableViewController : UITableViewController
+
+@property (nonatomic) NELLyricController *lyricController;
 
 @end
 
--- LyricFinder-Objc/LyricFinder-Objc/View Controllers/NELSongsTableViewController.m
+++ LyricFinder-Objc/LyricFinder-Objc/View Controllers/NELSongsTableViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "NELSongsTableViewController.h"
+#import "NELLyricsSearchViewController.h"
+#import "NELLyricController.h"
+#import "NELLyric.h"
 
 @interface NELSongsTableViewController ()
 
@@ -20,30 +23,63 @@
    
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear: YES];
+    [self.tableView reloadData];
+}
+
+
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 
-    return 0;
+    return self.lyricController.songs.count;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"lyricCell" forIndexPath:indexPath];
     
+    NELLyric *songs = self.lyricController.songs[indexPath.row];
+    cell.textLabel.text = songs.title;
+    cell.detailTextLabel.text = songs.artist;
     
     
     return cell;
 }
 
-
-
 #pragma mark - Navigation
 
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-   
+    
+    if ([segue.identifier isEqualToString:@"fromCell"]) {
+        //Passing the task and task controller
+        NELLyricsSearchViewController *destinationVC = segue.destinationViewController;
+        
+        NSIndexPath *indexPath = self.tableView.indexPathForSelectedRow;
+        
+        destinationVC.lyricController = self.lyricController;
+        destinationVC.song = self.lyricController.songs[indexPath.row];
+    } else if ([segue.identifier isEqualToString:@"fromBarButton"]) {
+        
+        //Create a task only need task controller.
+        NELLyricsSearchViewController *destinationVC = segue.destinationViewController;
+        destinationVC.lyricController = self.lyricController;
+    }
 }
 
+
+@synthesize lyricController = _lyricController;
+
+//the getter for lyricController
+- (NELLyricController *)lyricController
+{
+    if (!_lyricController) {
+        _lyricController = [[NELLyricController alloc] init];
+    }
+    return _lyricController;
+}
 
 @end

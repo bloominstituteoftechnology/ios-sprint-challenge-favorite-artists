1f9089c4d38737babbf4841e9b22aec05fca5568
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-05 11:43:47 -0400

    build model

---
+++ FavoriteArtists/FavoriteArtists/GLIArtistDetail.m
@@ -0,0 +1,15 @@
+//
+//  GLIArtistDetail.m
+//  FavoriteArtists
+//
+//  Created by Julian A. Fordyce on 4/5/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import "GLIArtistDetail.h"
+
+@implementation GLIArtistDetail
+
+
+
+@end

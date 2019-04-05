a3b2b1bfe6bb31f121d29a59218d699e337ae8bf
Frulwinn <frulwinn@gmail.com>
2019-04-05 10:04:35 -0500

    initial commit

---
+++ Lyric Finder/Lyric Finder/View Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lyric Finder
+//
+//  Created by Lambda_School_Loaner_34 on 4/5/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lyric Finder/Lyric Finder/View Controllers/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Lyric Finder
+//
+//  Created by Lambda_School_Loaner_34 on 4/5/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

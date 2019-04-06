6af537e8ce6f8f0c075db7c736b2ac3459843592
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-06 05:30:41 -0400

    create table view class and set scene to it

---
+++ FavoriteArtists/FavoriteArtists/ArtistDetailTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ArtistDetailTableViewController.h
+//  FavoriteArtists
+//
+//  Created by Julian A. Fordyce on 4/6/19.
+//  Copyright © 2019 Julian A. Fordyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistDetailTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

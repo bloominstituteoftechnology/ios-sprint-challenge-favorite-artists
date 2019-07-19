1b7519a942125e2b5f99f1456a6ff22c9fa8aea7
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-19 09:06:00 -0600

    Initial commit with new Xcode project

---
+++ Artists/Artists/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Artists
+//
+//  Created by Mitchell Budge on 7/19/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

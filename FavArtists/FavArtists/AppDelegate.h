5a052edccbcafaee76237aeae9736a922403be6e
victor <ruizdvictor@gmail.com>
2019-07-19 14:37:16 -0400

    added model logic

---
+++ FavArtists/FavArtists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavArtists
+//
+//  Created by Victor  on 7/19/19.
+//  Copyright © 2019 Victor . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

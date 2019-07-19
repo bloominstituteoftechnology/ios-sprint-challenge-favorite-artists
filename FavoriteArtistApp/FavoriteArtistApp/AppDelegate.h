a6501d51f21738430c292be1b9112b39afb33c7d
dllewisjolley <dlewisjolley@gmail.com>
2019-07-19 11:15:28 -0400

    Intial Commit

---
+++ FavoriteArtistApp/FavoriteArtistApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoriteArtistApp
+//
+//  Created by Diante Lewis-Jolley on 7/19/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

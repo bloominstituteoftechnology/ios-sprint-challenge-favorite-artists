2f9110b3b2f019542974343ff7384cc3a71e16a4
Christopher Aronson <christopheraronson@icloud.com>
2019-07-19 09:05:58 -0700

    Created UI

---
+++ Favorite Artists/Favorite Artists/View Controllers/KRCDetailViewController.h
@@ -0,0 +1,22 @@
+//
+//  KRCDetailViewController.h
+//  Favorite Artists
+//
+//  Created by Christopher Aronson on 7/19/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KRCDetailViewController : UIViewController
+
+//@property (nonatomic) KRCPokemonController *pokemonController;
+//@property KRCPokemon *pokemon;
+//
+//- (void)setPokemonController:(KRCPokemonController * _Nonnull)pokemonController;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/View Controllers/KRCDetailViewController.m
@@ -0,0 +1,27 @@
+//
+//  KRCDetailViewController.m
+//  Favorite Artists
+//
+//  Created by Christopher Aronson on 7/19/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+#import "KRCDetailViewController.h"
+
+@interface KRCDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *artistLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearLabel;
+@property (weak, nonatomic) IBOutlet UITextView *bioTextView;
+
+
+@end
+
+@implementation KRCDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end

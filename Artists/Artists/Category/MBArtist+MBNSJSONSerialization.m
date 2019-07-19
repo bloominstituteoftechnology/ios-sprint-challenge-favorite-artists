678d76f0b05b917e7f47cf35f12fa0a96eb55a00
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-19 09:25:21 -0600

    Implement NSJSONSerialization category

---
+++ Artists/Artists/Category/MBArtist+MBNSJSONSerialization.m
@@ -0,0 +1,33 @@
+//
+//  MBArtist+MBNSJSONSerialization.m
+//  Artists
+//
+//  Created by Mitchell Budge on 7/19/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+#import "MBArtist+MBNSJSONSerialization.h"
+
+@implementation MBArtist (MBNSJSONSerialization)
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        self.artist = [dictionary objectForKey:@"strArtist"];
+        self.bio = [dictionary objectForKey:@"strBio"];
+        self.year = [[dictionary objectForKey:@"intYear"] intValue];
+    }
+    return self;
+}
+
+- (NSDictionary *)artistDictionary {
+    NSMutableDictionary *artistDictionary = [NSMutableDictionary dictionary];
+    [artistDictionary setObject:self.artist forKey:@"strArtist"];
+    [artistDictionary setObject:self.bio forKey:@"strBio"];
+    NSString *yearFormedSteing = @(self.year).stringValue;
+    [artistDictionary setObject:yearFormedSteing forKey:@"intYear"];
+    NSDictionary *convertedDictionary = [NSDictionary dictionaryWithObjectsAndKeys:self.artist, @"strArtist", self.bio, @"strBio", self.year, @"intYear", nil];
+    return convertedDictionary;
+}
+
+@end

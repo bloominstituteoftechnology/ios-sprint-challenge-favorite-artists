ff28b4d78e60d8a7084e6e7dac2187c1a28de34b
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-19 10:26:59 -0600

    Fixed storyboard and added 3 ViewController files.

---
+++ Artist/Artist/View Controllers/HHArtistDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  HHArtistDetailViewController.m
+//  Artist
+//
+//  Created by Hayden Hastings on 7/19/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import "HHArtistDetailViewController.h"
+
+@interface HHArtistDetailViewController ()
+
+@end
+
+@implementation HHArtistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Artist/Artist/View Controllers/HHArtistTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  HHArtistTableViewController.h
+//  Artist
+//
+//  Created by Hayden Hastings on 7/19/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HHArtistTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

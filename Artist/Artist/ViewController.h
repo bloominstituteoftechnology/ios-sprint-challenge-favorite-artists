9528eea46c98993f217fc355ccdfe584e378f983
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-19 09:05:22 -0600

    Initial commit with storyboard done.

---
+++ Artist/Artist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Artist
+//
+//  Created by Hayden Hastings on 7/19/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Artist/Artist/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Artist
+//
+//  Created by Hayden Hastings on 7/19/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

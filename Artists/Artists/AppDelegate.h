94803c48231fc8da78f9c35688f9c2dbf3feeac4
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-19 08:06:44 -0700

    init

---
+++ Artists/Artists/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Artists
+//
+//  Created by Hector Steven on 7/19/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Artists/Artists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Artists
+//
+//  Created by Hector Steven on 7/19/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

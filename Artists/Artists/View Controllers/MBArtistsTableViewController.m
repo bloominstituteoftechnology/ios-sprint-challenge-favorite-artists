8ad8cb048eb80ec38395931893d5f094034da7cd
Mitchell Budge <mitchellbudge@gmail.com>
2019-07-19 10:13:22 -0600

    Update MBArtistsTableViewController.m

--- Artists/Artists/View Controllers/MBArtistsTableViewController.m
+++ Artists/Artists/View Controllers/MBArtistsTableViewController.m
@@ -16,12 +16,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table view data source

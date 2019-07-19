31aa40abc9124d01b2a914f82946445f732e1d80
hectorsvill <hectorstevenvillasano@gmail.com>
2019-07-19 08:24:21 -0700

    tableview connected

--- Artists/Artists/View Controller /HSVArtistsTableViewController.m
+++ Artists/Artists/View Controller /HSVArtistsTableViewController.m
@@ -26,25 +26,21 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return 10;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+	
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SongCell" forIndexPath:indexPath];
     
-    // Configure the cell...
+	cell.textLabel.text = @"cell";
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.

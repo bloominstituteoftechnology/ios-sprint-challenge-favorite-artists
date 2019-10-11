4f5f86d3d18abb24197242be815ff02e5dd13887
Michael Redig <mredig@gmail.com>
2019-10-11 11:09:46 -0500

    added core data convenience init

--- Favorite Artists ObjC/Favorite Artists ObjC/Model/REPArtist+REPConvenience.h
+++ Favorite Artists ObjC/Favorite Artists ObjC/Model/REPArtist+REPConvenience.h
@@ -11,5 +11,7 @@
 
 @interface REPArtist (REPConvenience)
 
+- (instancetype)initWithArtistNamed:(NSString *)name yearFormed:(NSString *)yearFormed withID:(NSString *)artistID mood:(NSString *)mood genre:(NSString *)genre bio:(NSString *)bio onContext:(NSManagedObjectContext *)context;
+
 @end
 
--- Favorite Artists ObjC/Favorite Artists ObjC/Model/REPArtist+REPConvenience.m
+++ Favorite Artists ObjC/Favorite Artists ObjC/Model/REPArtist+REPConvenience.m
@@ -11,4 +11,16 @@
 
 @implementation REPArtist (REPConvenience)
 
+- (instancetype)initWithArtistNamed:(NSString *)name yearFormed:(NSString *)yearFormed withID:(NSString *)artistID mood:(NSString *)mood genre:(NSString *)genre bio:(NSString *)bio onContext:(NSManagedObjectContext *)context {
+	if (self = [super initWithContext:context]) {
+		self.name = name;
+		self.yearFormed = yearFormed;
+		self.id = artistID;
+		self.mood = mood;
+		self.genre = genre;
+		self.bio = bio;
+	}
+	return self;
+}
+
 @end

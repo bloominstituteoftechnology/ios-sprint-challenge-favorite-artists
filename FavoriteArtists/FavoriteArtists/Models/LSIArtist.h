170acc1dc8a2e911913094624dc28ec75fc0a94b
luqmaan <luq.j.khan@gmail.com>
2019-10-11 10:46:39 -0500

    add properties to artist model and forward declare artist class

--- FavoriteArtists/FavoriteArtists/Models/LSIArtist.h
+++ FavoriteArtists/FavoriteArtists/Models/LSIArtist.h
@@ -8,10 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
+@class Artist;
 
 @interface LSIArtist : NSObject
 
+@property NSString *artistName;
+@property NSString *artistBio;
+@property int formedYear;
+
 @end
 
-NS_ASSUME_NONNULL_END
+

3dd159f7f63d1a2c49244da8bcaee73ed468f1ac
luqmaan <luq.j.khan@gmail.com>
2019-10-11 11:58:13 -0500

    add forward declaration of artist controller and artist controlller property

--- FavoriteArtists/FavoriteArtists/ViewControllers/LSIArtistTableViewController.h
+++ FavoriteArtists/FavoriteArtists/ViewControllers/LSIArtistTableViewController.h
@@ -8,8 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+@class LSIArtistController;
 
 @interface LSIArtistTableViewController : UITableViewController
+
+@property (nonatomic)LSIArtistController *controller;
 
 @end
 

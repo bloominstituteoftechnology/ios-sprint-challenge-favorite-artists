447da9bab7a018208e7cba40b2324c1dcc161ced
luqmaan <luq.j.khan@gmail.com>
2019-10-11 10:43:49 -0500

    add artist model file subclassed NSObject

---
+++ FavoriteArtists/FavoriteArtists/LSIArtist.h
@@ -0,0 +1,17 @@
+//
+//  LSIArtist.h
+//  FavoriteArtists
+//
+//  Created by Luqmaan Khan on 10/11/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIArtist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/LSIArtist.m
@@ -0,0 +1,13 @@
+//
+//  LSIArtist.m
+//  FavoriteArtists
+//
+//  Created by Luqmaan Khan on 10/11/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import "LSIArtist.h"
+
+@implementation LSIArtist
+
+@end

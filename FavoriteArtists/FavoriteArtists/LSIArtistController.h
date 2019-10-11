d5d8f7e65889fda914af04ce22be8e77eaf5e7b3
luqmaan <luq.j.khan@gmail.com>
2019-10-11 11:10:19 -0500

    add LSIArtistController files

---
+++ FavoriteArtists/FavoriteArtists/LSIArtistController.h
@@ -0,0 +1,15 @@
+//
+//  LSIArtistController.h
+//  FavoriteArtists
+//
+//  Created by Luqmaan Khan on 10/11/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface LSIArtistController : NSObject
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/LSIArtistController.m
@@ -0,0 +1,13 @@
+//
+//  LSIArtistController.m
+//  FavoriteArtists
+//
+//  Created by Luqmaan Khan on 10/11/19.
+//  Copyright © 2019 Luqmaan Khan. All rights reserved.
+//
+
+#import "LSIArtistController.h"
+
+@implementation LSIArtistController
+
+@end

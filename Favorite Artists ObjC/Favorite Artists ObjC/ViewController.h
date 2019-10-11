063a276e4b7f76e31069df32353378ed8c8ff83d
Michael Redig <mredig@gmail.com>
2019-10-11 10:24:51 -0500

    added project

---
+++ Favorite Artists ObjC/Favorite Artists ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Artists ObjC
+//
+//  Created by Michael Redig on 10/11/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Favorite Artists ObjC/Favorite Artists ObjC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Favorite Artists ObjC
+//
+//  Created by Michael Redig on 10/11/19.
+//  Copyright © 2019 Red_Egg Productions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+
+@end

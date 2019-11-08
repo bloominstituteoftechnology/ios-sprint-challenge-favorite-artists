a501e2e01a7bda8e766c661ef3c9b245a6679e59
Jake <jlconnerly@gmail.com>
2019-11-08 09:46:29 -0600

    Initial Commit

---
+++ Favorite-Artists-ST/Favorite-Artists-ST/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Favorite-Artists-ST
+//
+//  Created by Jake Connerly on 11/8/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}

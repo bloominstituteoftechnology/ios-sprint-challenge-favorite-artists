22c62a1d7a7be5847cc2e347b64fdb1ddb68c395
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-11-09 12:02:25 -0500

    Added Project

---
+++ Favorite Artists/Favorite Artists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Artists
+//
+//  Created by Michael Stoffer on 11/9/19.
+//  Copyright © 2019 Michael Stoffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

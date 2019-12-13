513b7849f1bd162b5d1e4f79ba9becf80c088373
Isvvc <lyons@tuta.io>
2019-12-13 09:43:44 -0700

    Implement initWithDictionary and toDictionary in LSIArtist

--- Favorite Artists/Favorite Artists/LSIArtist.m
+++ Favorite Artists/Favorite Artists/LSIArtist.m
@@ -10,4 +10,40 @@
 
 @implementation LSIArtist
 
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    if (self = [super init]) {
+        NSString *name = dictionary[@"strArtist"];
+        NSString *biography = dictionary[@"strBiographyEN"];
+        NSString *yearString = dictionary[@"intFormedYear"];
+        
+        _name = name;
+        _biography = biography;
+        _year = 0;
+        
+        if (yearString) {
+            NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+            [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+            
+            NSNumber *yearNumber = [formatter numberFromString:yearString];
+            if(yearNumber) {
+                _year = [yearNumber intValue];
+            }
+        }
+    }
+    return self;
+}
+
+- (NSDictionary *)toDictionary {
+    NSDictionary *dictionary = [[NSDictionary alloc] init];
+    
+    [dictionary setValue:self.name forKey:@"strArtist"];
+    [dictionary setValue:self.biography forKey:@"strBiographyEN"];
+    
+    if (self.year > 0) {
+        [dictionary setValue:[NSString stringWithFormat:@"%d", self.year] forKey:@"intFormedYear"];
+    }
+    
+    return dictionary;
+}
+
 @end

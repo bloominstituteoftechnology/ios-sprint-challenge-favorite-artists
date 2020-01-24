86c40074d4400428e638f3382c1fedb3318a0da0
Nar Kumar <njn.kumar@gmail.com>
2020-01-24 08:40:35 -0800

    Initial Commit

---
+++ Favorite Artist ObjC/Favorite Artist ObjC/Other/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favorite Artist ObjC
+//
+//  Created by Niranjan Kumar on 1/24/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Favorite Artist ObjC/Favorite Artist ObjCTests/Favorite_Artist_ObjCTests.m
@@ -0,0 +1,37 @@
+//
+//  Favorite_Artist_ObjCTests.m
+//  Favorite Artist ObjCTests
+//
+//  Created by Niranjan Kumar on 1/24/20.
+//  Copyright © 2020 Nar Kumar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Favorite_Artist_ObjCTests : XCTestCase
+
+@end
+
+@implementation Favorite_Artist_ObjCTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

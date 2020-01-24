e090c907dd8f5ea0478e44e69b928a1e47351b27
brian <bfvilchez95@gmail.com>
2020-01-24 09:08:33 -0700

    create starter project

---
+++ FavoriteAritists/FavoriteAritists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteAritists
+//
+//  Created by brian vilchez on 1/24/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+

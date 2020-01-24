68eb1578f59ee6e03e6536859d494b66aa842a4f
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-24 08:25:18 -0800

    Added Artist tableview controller class.

---
+++ unit4-sprintchallenge2-ss/unit4-sprintchallenge2-ss/SKSArtistTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  SKSArtistTableViewController.h
+//  unit4-sprintchallenge2-ss
+//
+//  Created by Lambda_School_Loaner_204 on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SKSArtistTableViewController : UITableViewController
+
+@end
+
---
+++ unit4-sprintchallenge2-ss/unit4-sprintchallenge2-ss/SKSArtistTableViewController.m
@@ -0,0 +1,64 @@
+//
+//  SKSArtistTableViewController.m
+//  unit4-sprintchallenge2-ss
+//
+//  Created by Lambda_School_Loaner_204 on 1/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "SKSArtistTableViewController.h"
+
+@interface SKSArtistTableViewController ()
+
+@end
+
+@implementation SKSArtistTableViewController
+
+static NSString * const reuseIdentifier = @"ArtistCell";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+
+@end

b8490ddfb68a4543f32f09112e0e9087369c9258
Jon Bash <jonbash@protonmail.com>
2020-01-24 08:06:59 -0800

    init xcode proj

---
+++ Unit4Sprint2Challenge/Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Unit4Sprint2Challenge
+//
+//  Created by Jon Bash on 2020-01-24.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Unit4Sprint2Challenge/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Unit4Sprint2Challenge
+//
+//  Created by Jon Bash on 2020-01-24.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

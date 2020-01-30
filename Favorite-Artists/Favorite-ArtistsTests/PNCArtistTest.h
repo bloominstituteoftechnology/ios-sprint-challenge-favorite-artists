ed3794c44d43f8f4f94a0f9807280b5f55470953
Percy Ngan <percival.ngan@gmail.com>
2020-01-30 08:45:50 -0600

    Added PNCArtistTest files load json from bundle

---
+++ Favorite-Artists/Favorite-ArtistsTests/PNCArtistTest.h
@@ -0,0 +1,17 @@
+//
+//  PNCArtistTest.h
+//  Favorite Artist3
+//
+//  Created by Lambda_School_Loaner_263 on 1/28/20.
+//  Copyright © 2020 Lamdba School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PNCArtistTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END

8f1c4d98029c8a30721517c57ffac6d6b6c99534
Joel Groomer <joelgroomer@gmail.com>
2020-02-08 09:03:14 -0800

    Xcode project created

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Joel Groomer on 2/8/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Joel Groomer on 2/8/20.
+//  Copyright © 2020 Julltron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

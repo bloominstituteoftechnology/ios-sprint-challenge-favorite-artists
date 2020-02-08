5c221b6ccce7a8093c4029389384061bfea4fb27
John Kouris <jdkouris@gmail.com>
2020-02-08 09:34:51 -0800

    Added the artist model

---
+++ FavoriteArtist/FavoriteArtist/JDKArtist.h
@@ -0,0 +1,21 @@
+//
+//  JDKArtist.h
+//  FavoriteArtist
+//
+//  Created by John Kouris on 2/8/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JDKArtist : NSObject
+
+@property NSString *name;
+@property NSString *biography;
+@property int yearFormed;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtist/FavoriteArtist/JDKArtist.m
@@ -0,0 +1,13 @@
+//
+//  JDKArtist.m
+//  FavoriteArtist
+//
+//  Created by John Kouris on 2/8/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import "JDKArtist.h"
+
+@implementation JDKArtist
+
+@end

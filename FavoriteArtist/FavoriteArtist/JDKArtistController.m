f25b7e4b95f101d1103de7b4c7d4062250b8da26
John Kouris <jdkouris@gmail.com>
2020-02-08 09:50:42 -0800

    Added the artist controller

---
+++ FavoriteArtist/FavoriteArtist/JDKArtistController.m
@@ -0,0 +1,13 @@
+//
+//  JDKArtistController.m
+//  FavoriteArtist
+//
+//  Created by John Kouris on 2/8/20.
+//  Copyright © 2020 John Kouris. All rights reserved.
+//
+
+#import "JDKArtistController.h"
+
+@implementation JDKArtistController
+
+@end

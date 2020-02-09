f3a70b91fff4f202eab967eb492f9b222613113f
Vici Shaweddy <vshaweddy@gmail.com>
2020-02-09 16:09:47 -0500

    setting up the storyboard

---
+++ Favorite Artists/Favorite Artists/Models/VVSArtist.h
@@ -0,0 +1,14 @@
+//
+//  VVSArtist.h
+//  Favorite Artists
+//
+//  Created by Vici Shaweddy on 2/7/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VVSArtist : NSObject
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/Models/VVSArtist.m
@@ -0,0 +1,13 @@
+//
+//  VVSArtist.m
+//  Favorite Artists
+//
+//  Created by Vici Shaweddy on 2/7/20.
+//  Copyright © 2020 Vici Shaweddy. All rights reserved.
+//
+
+#import "VVSArtist.h"
+
+@implementation VVSArtist 
+
+@end

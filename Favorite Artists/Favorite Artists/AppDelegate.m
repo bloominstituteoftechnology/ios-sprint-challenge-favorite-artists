45d6981a66b85101b123e0553620958bd90f37ae
Rick Wolter <richardx1016@gmail.com>
2020-02-21 11:27:01 -0500

    Proj started :)

---
+++ Favorite Artists/Favorite Artists/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  Favorite Artists
+//
+//  Created by Rick Wolter on 2/21/20.
+//  Copyright © 2020 Devshop7. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ Favorite Artists/Favorite Artists/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Favorite Artists
+//
+//  Created by Rick Wolter on 2/21/20.
+//  Copyright © 2020 Devshop7. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end

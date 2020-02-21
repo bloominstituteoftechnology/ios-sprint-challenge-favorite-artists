1b7259130c176e0002fd76fc6621a95c61da8a33
brian <bfvilchez95@gmail.com>
2020-02-21 09:10:35 -0700

    created ios 12 Project

---
+++ FavoriteArtists(iOS12)/FavoriteArtists(iOS12)/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists(iOS12)
+//
+//  Created by brian vilchez on 2/21/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists(iOS12)/FavoriteArtists(iOS12)/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  FavoriteArtists(iOS12)
+//
+//  Created by brian vilchez on 2/21/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}

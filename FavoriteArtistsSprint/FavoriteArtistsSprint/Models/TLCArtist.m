f7b18dd5c659af8bde4244617edbe3d581706ca6
Troy Christian <apostasy8@gmail.com>
2020-02-21 11:55:40 -0500

    add artist

---
+++ FavoriteArtistsSprint/FavoriteArtistsSprint/Models/TLCArtist.m
@@ -0,0 +1,25 @@
+//
+//  TLCArtist.m
+//  FavoriteArtistsSprint
+//
+//  Created by Lambda_School_Loaner_219 on 2/21/20.
+//  Copyright © 2020 Lambda_School_Loaner_219. All rights reserved.
+//
+
+#import "TLCArtist.h"
+
+@implementation TLCArtist
+
+-(instancetype)initWithName:(NSString *)name artistBio:(NSString *)artistBio yearFormed:(int)yearFormed {
+    self = [self init];
+    if (self) {
+        _name = name;
+        _artistBio = artistBio;
+        _yearFormed = yearFormed;
+    }
+    return self;
+}
+
+
+
+@end

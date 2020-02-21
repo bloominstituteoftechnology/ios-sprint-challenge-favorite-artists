79d06cd4fbb83701b127300704d2f8022bf30d9e
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-21 13:16:56 -0500

    Set up Table View Controller with Fetched Results Controller

---
+++ FavoriteArtists/FavoriteArtists/Controller/View Controllers/TableView/CARFavoriteArtistsTableViewController.h
@@ -0,0 +1,18 @@
+//
+//  CARFavoriteArtistsTableViewController.h
+//  FavoriteArtists
+//
+//  Created by Chad Rutherford on 2/21/20.
+//  Copyright © 2020 Chad Rutherford. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CARFavoriteArtistsTableViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END

db7a5e07bc093df8a201c73e1e1f48f623875ddf
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-03-20 08:07:27 -0700

    added new project

---
+++ FavoriteArtistsSprint/FavoriteArtistsSprint/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtistsSprint
+//
+//  Created by Jorge Alvarez on 3/20/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtistsSprint/FavoriteArtistsSprint/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtistsSprint
+//
+//  Created by Jorge Alvarez on 3/20/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

4c64ae221909f3d0884aea8d6f10d27db42e1761
Aaron Cleveland <toscleveland@gmail.com>
2020-03-20 10:44:13 -0500

    init project

---
+++ FavArtist/FavArtist/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavArtist
+//
+//  Created by Aaron Cleveland on 3/20/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavArtist/FavArtist/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavArtist
+//
+//  Created by Aaron Cleveland on 3/20/20.
+//  Copyright © 2020 Aaron Cleveland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

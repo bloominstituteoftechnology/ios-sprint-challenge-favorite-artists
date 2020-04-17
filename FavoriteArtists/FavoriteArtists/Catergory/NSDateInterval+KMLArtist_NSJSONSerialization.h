6488d0cadf3ee96d2b8304f39f5e278b15b05b33
Keri Levesque <kerianne166@gmail.com>
2020-04-17 11:56:11 -0400

    added catergory file

---
+++ FavoriteArtists/FavoriteArtists/Catergory/NSDateInterval+KMLArtist_NSJSONSerialization.h
@@ -0,0 +1,17 @@
+//
+//  NSDateInterval+KMLArtist_NSJSONSerialization.h
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDateInterval (KMLArtist_NSJSONSerialization)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Catergory/NSDateInterval+KMLArtist_NSJSONSerialization.m
@@ -0,0 +1,13 @@
+//
+//  NSDateInterval+KMLArtist_NSJSONSerialization.m
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import "NSDateInterval+KMLArtist_NSJSONSerialization.h"
+
+@implementation NSDateInterval (KMLArtist_NSJSONSerialization)
+
+@end

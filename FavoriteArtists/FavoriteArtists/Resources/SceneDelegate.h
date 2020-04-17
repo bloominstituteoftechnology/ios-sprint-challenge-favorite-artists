6a97cb2d90518ec91ab5dec232898225d33f29a9
Chris <chris.gonzales.82@gmail.com>
2020-04-17 08:30:32 -0700

    Stubbed out folder, created table view, and detail view scenes.

---
+++ FavoriteArtists/FavoriteArtists/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Chris Gonzales on 4/17/20.
+//  Copyright © 2020 Chris Gonzales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

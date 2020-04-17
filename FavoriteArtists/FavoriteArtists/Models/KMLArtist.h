25a9ffff8b3b4628bc3682816541f2bfe1ae3ed2
Keri Levesque <kerianne166@gmail.com>
2020-04-17 11:32:20 -0400

    added model file

---
+++ FavoriteArtists/FavoriteArtists/Models/KMLArtist.h
@@ -0,0 +1,17 @@
+//
+//  KMLArtist.h
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KMLArtist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Models/KMLArtist.m
@@ -0,0 +1,13 @@
+//
+//  KMLArtist.m
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import "KMLArtist.h"
+
+@implementation KMLArtist
+
+@end

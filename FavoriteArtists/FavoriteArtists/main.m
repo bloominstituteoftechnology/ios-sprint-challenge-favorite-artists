524f9b156a008a61b8f67bc9f968dc669e8ff19e
Keri Levesque <kerianne166@gmail.com>
2020-04-17 11:06:12 -0400

    added starter project

---
+++ FavoriteArtists/FavoriteArtists/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

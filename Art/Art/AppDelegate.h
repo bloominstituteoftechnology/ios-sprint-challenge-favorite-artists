41082b75dbb71df4ed0e1c3d0a35fc99b334731c
Nick <ptnguyen1901@gmail.com>
2020-04-17 11:28:52 -0400

    Set up UI.

---
+++ Art/Art/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Art
+//
+//  Created by Nick Nguyen on 4/17/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Art/Art/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Art
+//
+//  Created by Nick Nguyen on 4/17/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

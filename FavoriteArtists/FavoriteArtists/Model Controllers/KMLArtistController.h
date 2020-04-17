3f7f57b476de73c91dda293f206a481e9aca5d0c
Keri Levesque <kerianne166@gmail.com>
2020-04-17 11:38:53 -0400

    added the model controller file

---
+++ FavoriteArtists/FavoriteArtists/Model Controllers/KMLArtistController.h
@@ -0,0 +1,17 @@
+//
+//  KMLArtistController.h
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KMLArtistController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Model Controllers/KMLArtistController.m
@@ -0,0 +1,13 @@
+//
+//  KMLArtistController.m
+//  FavoriteArtists
+//
+//  Created by Keri Levesque on 4/17/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+#import "KMLArtistController.h"
+
+@implementation KMLArtistController
+
+@end

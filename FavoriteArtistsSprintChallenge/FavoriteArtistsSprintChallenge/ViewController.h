a7f271901fb27f9c556d1004950b4b3d802084c4
dillonp23 <dillonp.lambda@gmail.com>
2020-04-19 18:10:54 -0700

    Added Xcode Starter Project

---
+++ FavoriteArtistsSprintChallenge/FavoriteArtistsSprintChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtistsSprintChallenge
+//
+//  Created by Dillon P on 4/19/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoriteArtistsSprintChallenge/FavoriteArtistsSprintChallenge/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtistsSprintChallenge
+//
+//  Created by Dillon P on 4/19/20.
+//  Copyright © 2020 Dillon's Lambda Team. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

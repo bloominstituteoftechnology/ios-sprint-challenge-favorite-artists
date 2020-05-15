e6dfde4322cffa42b43cbba272738d2661db4abd
Tobi Tselios <tobi.tselios@gmail.com>
2020-05-15 18:18:02 +0100

    Find Artist Network Call

---
+++ FavoriteArtists/FavoriteArtists/OTKArtistController.h
@@ -0,0 +1,23 @@
+//
+//  OTKArtistController.h
+//  FavoriteArtists
+//
+//  Created by Tobi Kuyoro on 15/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OTKArtist.h"
+
+typedef void(^ArtistFetchCompletion)(OTKArtist * _Nullable artist, NSError * _Nullable error);
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OTKArtistController : NSObject
+
+- (void)findArtist:(NSString *)artistName completion:(ArtistFetchCompletion)completion;
+- (void)addArtist:(OTKArtist *)artist;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/OTKArtistController.m
@@ -0,0 +1,51 @@
+//
+//  OTKArtistController.m
+//  FavoriteArtists
+//
+//  Created by Tobi Kuyoro on 15/05/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+#import "OTKArtistController.h"
+#import "OTKArtist+NSJSONSerialization.h"
+#import "OTKArtist.h"
+
+static NSString *baseURLString = @"https://theaudiodb.com/api/v1/json/1/search.php";
+
+@implementation OTKArtistController
+
+- (void)findArtist:(NSString *)artistName completion:(ArtistFetchCompletion)completion {
+    NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithString:baseURLString];
+    NSURLQueryItem *artist = [NSURLQueryItem queryItemWithName:@"s" value:artistName];
+    urlComponents.queryItems = @[artist];
+
+    NSURL *url = urlComponents.URL;
+
+    NSURLSessionDataTask *task = [NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            completion(nil, error);
+            return;
+        }
+
+        if (!data) {
+            completion(nil, [[NSError alloc] init]);
+            return;
+        }
+
+        NSError *errorWithJSON = nil;
+
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&errorWithJSON];
+        if (errorWithJSON) {
+            completion(nil, error);
+            return;
+        }
+
+        NSDictionary *artistJSON = [json[@"artists"] firstObject];
+        OTKArtist *artist = [[OTKArtist alloc] initWithDictionary:artistJSON];
+        completion(artist, nil);
+    }];
+
+    [task resume];
+}
+
+@end

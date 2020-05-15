dba870e17d11c791dc8c324fcea33738182b9e8f
Mark Gerrior <github@gerrior.com>
2020-05-15 09:08:39 -0700

    Added Artist Model

---
+++ Favorite Artists/Favorite Artists/Models/MTGArtist.h
@@ -0,0 +1,21 @@
+//
+//  MTGArtist.h
+//  Favorite Artists
+//
+//  Created by Mark Gerrior on 5/15/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTGArtist : NSObject
+
+@property (nonatomic, readonly, copy) NSString *artist;
+@property (nonatomic, readonly, copy) NSString *biography;
+@property (nonatomic, readonly) double formedYear;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/Models/MTGArtist.m
@@ -0,0 +1,13 @@
+//
+//  MTGArtistModel.m
+//  Favorite Artists
+//
+//  Created by Mark Gerrior on 5/15/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import "MTGArtist.h"
+
+@implementation MTGArtist
+
+@end

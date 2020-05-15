715f1b277c6cbff7f1e817f5814c4cbf7c897c2e
Mark Gerrior <github@gerrior.com>
2020-05-15 09:21:09 -0700

    Stub for NSJSONSerialization

---
+++ Favorite Artists/Favorite Artists/Models/MTGArtist+NSJSONSerialization.m
@@ -0,0 +1,21 @@
+//
+//  MTGArtist+NSJSONSerialization.m
+//  Favorite Artists
+//
+//  Created by Mark Gerrior on 5/15/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import "MTGArtist+NSJSONSerialization.h"
+
+@implementation MTGArtist (NSJSONSerialization)
+
++ (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    return nil;
+}
+
++ (NSDictionary *)toDictionary {
+    return nil;
+}
+
+@end

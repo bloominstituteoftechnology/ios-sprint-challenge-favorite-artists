bb7802c6ce27885ca1f50f75e72a942685f2c920
Hector <hectoremil21@hotmail.com>
2020-05-15 12:09:10 -0400

    Create Artist+NSJSONSerialization category files

---
+++ FavoriteArtists/FavoriteArtists/Artist+NSJSONSerialization.h
@@ -0,0 +1,17 @@
+//
+//  Artist+NSJSONSerialization.h
+//  FavoriteArtists
+//
+//  Created by Karen Rodriguez on 5/15/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import "Artist.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist (NSJSONSerialization)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Artist+NSJSONSerialization.m
@@ -0,0 +1,13 @@
+//
+//  Artist+NSJSONSerialization.m
+//  FavoriteArtists
+//
+//  Created by Karen Rodriguez on 5/15/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+#import "Artist+NSJSONSerialization.h"
+
+@implementation Artist (NSJSONSerialization)
+
+@end

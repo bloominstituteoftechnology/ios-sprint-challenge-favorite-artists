d1d9533bb069ddf9cbe30fd730bdf292d030322d
Shawn Gee <shawngee@gmail.com>
2020-05-15 11:27:31 -0400

    File organization

---
+++ FavoriteArtists/FavoriteArtists/Networking/LSINetworkingClient.h
@@ -0,0 +1,17 @@
+//
+//  LSINetworkingClient.h
+//  FavoriteArtists
+//
+//  Created by Shawn Gee on 5/15/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSINetworkingClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Networking/LSINetworkingClient.m
@@ -0,0 +1,13 @@
+//
+//  LSINetworkingClient.m
+//  FavoriteArtists
+//
+//  Created by Shawn Gee on 5/15/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+#import "LSINetworkingClient.h"
+
+@implementation LSINetworkingClient
+
+@end

87aaf7b11eaece28168c066367ed220c275e84d7
Wyatt Harrell <wyattaharrell@gmail.com>
2020-05-15 11:04:17 -0400

    Created Xcode project

---
+++ Favorite Artists/Favorite Artists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Favorite Artists
+//
+//  Created by Wyatt Harrell on 5/15/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

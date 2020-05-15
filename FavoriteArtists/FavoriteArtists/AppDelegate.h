b5e35bf7e48865188183da585e6b87c9e208cb33
knopptimism <56057392+knopptimism@users.noreply.github.com>
2020-05-15 16:57:33 -0700

    Submission for deadline.

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Lambda_School_Loaner_268 on 5/15/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Lambda_School_Loaner_268 on 5/15/20.
+//  Copyright © 2020 Lambda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

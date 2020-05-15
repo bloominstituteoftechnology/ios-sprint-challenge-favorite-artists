229eea699ecab95fac2601f5dd54783db9049700
Shawn Gee <shawngee@gmail.com>
2020-05-15 11:21:23 -0400

    Initial project created

---
+++ FavoriteArtists/FavoriteArtists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Shawn Gee on 5/15/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Shawn Gee on 5/15/20.
+//  Copyright © 2020 Swift Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

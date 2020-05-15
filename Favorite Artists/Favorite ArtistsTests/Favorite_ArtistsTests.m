58111606b5351eda6ec610b7c2800054c384214c
Mark Gerrior <github@gerrior.com>
2020-05-15 11:23:56 -0700

    All 4 artist tests pass.

--- Favorite Artists/Favorite ArtistsTests/Favorite_ArtistsTests.m
+++ Favorite Artists/Favorite ArtistsTests/Favorite_ArtistsTests.m
@@ -47,4 +47,68 @@
     XCTAssertEqual(1999, artist.formedYear);
 }
 
+- (MTGArtist *)loadArtist:(NSString *)filename {
+    
+    // Load test object from file.
+    NSData *artistData = loadFile(filename, [MTGArtist class]);
+    NSLog(@"artistData: %@", artistData);
+
+    // Pass through JSON Serializer
+    NSError *jsonError = nil;
+    NSDictionary *artistDictionary = [NSJSONSerialization JSONObjectWithData:artistData
+                                                                     options:0
+                                                                       error:&jsonError];
+    if (jsonError) {
+        NSLog(@"JSON Parsing error: %@", jsonError);
+    }
+
+    NSLog(@"JSON: %@", artistDictionary);
+
+    // Parse the dictionary and turn it into a CurrentWeather object
+
+    // Pass it through MTGArtist initializer
+    MTGArtist *artist = [MTGArtist initWithDictionary:artistDictionary];
+
+    return artist;
+}
+
+- (void)testJohnWilliams {
+    NSString *filename = @"John Williams.json";
+    MTGArtist *artist = [self loadArtist:filename];
+
+    NSLog(@"artist: %@", artist);
+
+    XCTAssertEqualObjects(@"John Williams", artist.artist);
+// FIXME: Starts with?
+//    XCTAssertEqualObjects(@"John Towner Williams (born February 8, 1932) is an American composer", artist.biography);
+    XCTAssertEqual(-1, artist.formedYear);
+}
+
+- (void)testPinkFloyd {
+
+//    MTGArtist *artist = loadArtist(@"Pink Floyd.json");
+
+    NSString *filename = @"Pink Floyd.json";
+    MTGArtist *artist = [self loadArtist:filename];
+
+    NSLog(@"artist: %@", artist);
+
+    XCTAssertEqualObjects(@"Pink Floyd", artist.artist);
+// FIXME: Starts with?
+//    XCTAssertEqualObjects(@"Pink Floyd were an English rock band that achieved international success with their progressive and psychedelic rock music marked by the use of philosophical lyrics", artist.biography);
+    XCTAssertEqual(1965, artist.formedYear);
+}
+
+- (void)testZero7 {
+
+    NSString *filename = @"Zero 7.json";
+    MTGArtist *artist = [self loadArtist:filename];
+
+    NSLog(@"artist: %@", artist);
+
+    XCTAssertEqualObjects(@"Zero 7", artist.artist);
+// FIXME: Starts with?
+//    XCTAssertEqualObjects(@"Zero 7 is a British musical duo consisting of Henry Binns and Sam Hardaker.", artist.biography);
+    XCTAssertEqual(1997, artist.formedYear);
+}
 @end

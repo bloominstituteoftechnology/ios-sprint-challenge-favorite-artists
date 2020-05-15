ef78c6c5c6344a0a5003242d473d20aac6f3d374
Mark Gerrior <github@gerrior.com>
2020-05-15 08:19:10 -0700

    Initial Project

---
+++ Favorite Artists/Favorite Artists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Favorite Artists
+//
+//  Created by Mark Gerrior on 5/15/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Favorite Artists/Favorite Artists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Favorite Artists
+//
+//  Created by Mark Gerrior on 5/15/20.
+//  Copyright © 2020 Mark Gerrior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

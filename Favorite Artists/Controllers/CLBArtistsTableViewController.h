f90fae55078aa4a655a8fafc2c4b9edc7be041e8
Christian Lorenzo <clbmiami2004@gmail.com>
2020-05-30 12:55:14 -0700

    Created a fetching request for Artists.

---
+++ Favorite Artists/Controllers/CLBArtistsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  CLBArtistsTableViewController.h
+//  Favorite Artists
+//
+//  Created by Christian Lorenzo on 5/30/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLBArtistsTableViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Controllers/CLBArtistsTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  CLBArtistsTableViewController.m
+//  Favorite Artists
+//
+//  Created by Christian Lorenzo on 5/30/20.
+//  Copyright © 2020 Christian Lorenzo. All rights reserved.
+//
+
+#import "CLBArtistsTableViewController.h"
+
+@implementation CLBArtistsTableViewController
+
+@end

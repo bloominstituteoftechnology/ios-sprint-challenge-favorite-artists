d8c4954f0498ae7a1c710fbc3466d320b3ce48f3
Sal <salmanamer@gmail.com>
2020-05-29 18:56:02 -0700

    created NSJSONSerialization+SBAArtist.h

---
+++ Favorite Artists/Favorite Artists/NSJSONSerialization+SBAArtist.h
@@ -0,0 +1,17 @@
+//
+//  NSJSONSerialization+SBAArtist.h
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSJSONSerialization (SBAArtist)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/NSJSONSerialization+SBAArtist.m
@@ -0,0 +1,13 @@
+//
+//  NSJSONSerialization+SBAArtist.m
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import "NSJSONSerialization+SBAArtist.h"
+
+@implementation NSJSONSerialization (SBAArtist)
+
+@end

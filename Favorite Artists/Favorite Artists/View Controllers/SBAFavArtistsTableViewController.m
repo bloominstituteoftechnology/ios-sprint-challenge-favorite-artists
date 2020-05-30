c3a8064841faebf46dccd5274d5118767c4c3b59
Sal <salmanamer@gmail.com>
2020-05-29 20:47:27 -0700

    setup and tested dummy data for tableviewcontroller

--- Favorite Artists/Favorite Artists/View Controllers/SBAFavArtistsTableViewController.m
+++ Favorite Artists/Favorite Artists/View Controllers/SBAFavArtistsTableViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "SBAFavArtistsTableViewController.h"
+#import "SBAArtist.h"
+#import "SBAArtistModelController.h"
+#import "SBADetailSearchViewController.h"
+#import "SBAArtist+NSJSONSerialization.h"
 
 @interface SBAFavArtistsTableViewController ()
+@property (nonatomic) SBAArtistModelController *artistController;
+
 
 @end
 
@@ -16,6 +22,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.artistController = [[SBAArtistModelController alloc] init];
+  
+    // create test Artist
+  SBAArtist *testArtist = [[SBAArtist alloc] initWithArtistName:@"Bob Marley" biography:@"Jah Lives" yearFormed:1977];
+  [self.tableView reloadData];
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -27,67 +38,37 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+  return self.artistController.favoriteArtists.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ArtistCell" forIndexPath:indexPath];
     
-    // Configure the cell...
-    
-    return cell;
+  SBAArtist *artist = self.artistController.favoriteArtists[indexPath.row];
+  cell.textLabel.text = artist.name;
+  cell.detailTextLabel.text = [NSString stringWithFormat:@"Formed in %d", artist.yearFormed];
+  return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
+ 
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+  if ([segue.identifier isEqualToString:@"AddArtistsSegue"]) {
+    SBADetailSearchViewController *searchVC = [segue destinationViewController];
+    NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+//    searchVC.artist = self.artistController.favoriteArtists[indexPath.row];
+    
+  }
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+ 
 
 @end

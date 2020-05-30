85ae8da22c64d20ffcb55d27ff343dd9b31af8fe
Jessie <jessie.griffin@icloud.com>
2020-05-29 21:09:00 -0400

    Adds new project FavoriteArtists

---
+++ FavoriteArtists/FavoriteArtists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Jessie Ann Griffin on 5/29/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Jessie Ann Griffin on 5/29/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

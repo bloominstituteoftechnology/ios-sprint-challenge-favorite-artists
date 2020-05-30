20fba0a24c91f21e5980c5458cd15e113782d219
Sal <salmanamer@gmail.com>
2020-05-29 18:19:35 -0700

    setup artist model SBAArtist.h

---
+++ Favorite Artists/Favorite Artists/SBAArtist.m
@@ -0,0 +1,13 @@
+//
+//  SBAArtist.m
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import "SBAArtist.h"
+
+@implementation SBAArtist
+
+@end

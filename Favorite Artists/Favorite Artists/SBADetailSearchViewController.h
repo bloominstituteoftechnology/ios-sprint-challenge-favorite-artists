9e1205575714a3acfab1c068a20c4c7dca2ad960
Sal <salmanamer@gmail.com>
2020-05-29 18:38:11 -0700

    created SBADetailSearchViewController

---
+++ Favorite Artists/Favorite Artists/SBADetailSearchViewController.h
@@ -0,0 +1,17 @@
+//
+//  SBADetailSearchViewController.h
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBADetailSearchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/SBADetailSearchViewController.m
@@ -0,0 +1,32 @@
+//
+//  SBADetailSearchViewController.m
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import "SBADetailSearchViewController.h"
+
+@interface SBADetailSearchViewController ()
+
+@end
+
+@implementation SBADetailSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

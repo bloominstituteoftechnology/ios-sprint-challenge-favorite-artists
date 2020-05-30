745d8c393adec34794eeeb53b37619ed22e03616
Alex Thompson <guitarman226@gmail.com>
2020-05-30 12:49:54 -0500

    Storyboard layout, and model
    Completed storyboard layout and compled the model implentation file and header file

---
+++ FavoriteArtists/FavoriteArtists/LSIArtistsController.h
@@ -0,0 +1,17 @@
+//
+//  LSIArtistsController.h
+//  FavoriteArtists
+//
+//  Created by Alex Thompson on 5/30/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIArtistsController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
--- FavoriteArtists/FavoriteArtists/LSIArtistsController.m
+++ FavoriteArtists/FavoriteArtists/LSIArtistsController.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  LSIArtistsController.m
 //  FavoriteArtists
 //
 //  Created by Alex Thompson on 5/30/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "LSIArtistsController.h"
 
-@interface ViewController : UIViewController
-
+@implementation LSIArtistsController
 
 @end
-

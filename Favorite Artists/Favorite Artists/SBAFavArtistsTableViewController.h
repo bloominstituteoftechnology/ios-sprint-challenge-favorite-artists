27aa929a7225ecfac2b1248ef0e909e5cd273420
Sal <salmanamer@gmail.com>
2020-05-29 18:36:40 -0700

    created SBAArtistsTableViewController

---
+++ Favorite Artists/Favorite Artists/SBAFavArtistsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  SBAFavArtistsTableViewController.h
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBAFavArtistsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

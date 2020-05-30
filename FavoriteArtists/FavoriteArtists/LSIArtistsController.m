434d16380380c434ab7952cd6b371f0f2ddd2142
Alex Thompson <guitarman226@gmail.com>
2020-05-30 14:44:34 -0500

    Finished artistController
    implemented fetch function,  and trying to be able to save to the fileManager whenever the user saves a artists

--- FavoriteArtists/FavoriteArtists/LSIArtistsController.m
+++ FavoriteArtists/FavoriteArtists/LSIArtistsController.m
@@ -7,7 +7,86 @@
 //
 
 #import "LSIArtistsController.h"
+#import "LSIArtists.h"
+
+static NSString *baseURL = @"https://www.theaudiodb.com/api/v1/json/1/search.php";
 
 @implementation LSIArtistsController
+
+- (void)fetchArtistWithName:(NSString *)name
+                 completion:(ArtistFetcherCompletionHandler)completion {
+    
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithString:baseURL];
+    urlComponents.queryItems = @[
+        [NSURLQueryItem queryItemWithName:@"s" value:name]
+    ];
+    
+    NSURL *url = urlComponents.URL;
+    
+    if (!url) {
+        return;
+    }
+    
+    NSURLSessionTask * task = [NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            completion(nil, error);
+            return;
+        }
+        
+        if (!data) {
+            NSError *dataError = error;
+            completion(nil, dataError);
+            return;
+        }
+        
+        NSError *jsonError = nil;
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        
+        if (jsonError) {
+            completion(nil, jsonError);
+            return;
+        }
+        
+        if ([dictionary[@"artists"] isKindOfClass:[NSNull class]]) {
+            completion(nil, [[NSError alloc] init]);
+            return;
+        }
+        
+        NSDictionary *artist = [[dictionary objectForKey:@"artists"] firstObject];
+        if (artist == nil) {
+            completion(nil, [[NSError alloc] init]);
+        }
+        
+        LSIArtists *artistResults = [[LSIArtists alloc] initWithDictionary:artist];
+        completion(artistResults, nil);
+        
+    }];
+    [task resume];
+}
+
+- (NSArray *)fetchSavedArtists {
+    NSArray *path = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *directory = [path objectAtIndex:0];
+    NSArray *filePaths = [[NSFileManager defaultManager] subpathsOfDirectoryAtPath:directory error:nil];
+    
+    NSMutableArray *artists = [[NSMutableArray alloc] init];
+    
+    for (NSString *artist in filePaths) {
+        NSString *artistForPath = [NSHomeDirectory() stringByAppendingFormat:@"/Documents/%@", artist];
+        NSURL *artistURL = [NSURL fileURLWithPath:artistForPath];
+        
+        NSData *artistData = [[NSData alloc] initWithContentsOfURL:artistURL];
+        NSDictionary *artistDictionary = [NSJSONSerialization
+                                          JSONObjectWithData:artistData
+                                          options:0
+                                          error:nil];
+        
+        LSIArtists *artist = [[LSIArtists alloc] initWithDictionary:artistDictionary];
+        [artists addObject:artist];
+    }
+    
+    return artists;
+}
 
 @end

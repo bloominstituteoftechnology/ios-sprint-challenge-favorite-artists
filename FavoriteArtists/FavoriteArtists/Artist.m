e6244e47b2dc8c9a73314bdbc84913082c9b04a2
Jessie <jessie.griffin@icloud.com>
2020-05-29 21:28:03 -0400

    Adds model object Artist

---
+++ FavoriteArtists/FavoriteArtists/Artist.m
@@ -0,0 +1,23 @@
+//
+//  Artist.m
+//  FavoriteArtists
+//
+//  Created by Jessie Ann Griffin on 5/29/20.
+//  Copyright © 2020 Jessie Griffin. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+- (instancetype)initWithName:(NSString *)artistName biography:(NSString *)artistBiography yearFormed:(int)yearFormed
+{
+    if (self = [super init]) {
+        _artistName = artistName;
+        _artistBiography = artistBiography;
+        _yearFormed = yearFormed;
+    }
+    return self;
+}
+
+@end

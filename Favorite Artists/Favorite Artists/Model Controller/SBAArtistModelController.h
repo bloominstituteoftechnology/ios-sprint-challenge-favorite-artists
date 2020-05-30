84f30881cf7af13c75a2f89bea849e8e6b8eb15f
Sal <salmanamer@gmail.com>
2020-05-29 20:16:15 -0700

    Setup SBAArtistModelController header file

---
+++ Favorite Artists/Favorite Artists/Model Controller/SBAArtistModelController.h
@@ -0,0 +1,25 @@
+//
+//  SBAArtistModelController.h
+//  Favorite Artists
+//
+//  Created by Sal B Amer on 5/29/20.
+//  Copyright © 2020 Sal B AmerDEv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@class SBAArtist;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SBAArtistModelController : NSObject
+
+@property (nonatomic, readonly) NSMutableArray<SBAArtist *> *favoriteArtists;
+
+- (void)fetchArtistWithName:(NSString *)artistName
+            completionBlock:(void (^)(SBAArtist * _Nullable artist, NSError * _Nullable error))completionBlock;
+
+//TO DO Load from persistence here
+
+@end
+
+NS_ASSUME_NONNULL_END

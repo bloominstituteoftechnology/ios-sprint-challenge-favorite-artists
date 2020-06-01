3fe345774f094a1d406490fdcdc54705369d07ae
David Wright <davidtwright89@gmail.com>
2020-06-01 01:31:22 -0400

    Implemented the two ArtistResults initializers

--- Favorite Artists/Favorite Artists/ArtistResults.m
+++ Favorite Artists/Favorite Artists/ArtistResults.m
@@ -7,7 +7,40 @@
 //
 
 #import "ArtistResults.h"
+#import "Artist.h"
+#import "Artist+NSJSONSerialization.h"
 
 @implementation ArtistResults
+
+- (instancetype)initWithArtists:(NSArray<Artist *> *)artists
+{
+    if (self = [super init]) {
+        _artists = artists;
+    }
+    return self;
+}
+
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSArray *artistDictionaries = [dictionary objectForKey:@"artists"];
+    if (![artistDictionaries isKindOfClass:[NSArray class]]) return nil;
+    
+    NSMutableArray *artists = [[NSMutableArray alloc] initWithCapacity:artistDictionaries.count];
+    
+    for (NSDictionary *artistDictionary in artistDictionaries) {
+        if (![artistDictionary isKindOfClass:[NSDictionary class]]) continue;
+        
+        Artist *artist = [[Artist alloc] initWithDictionary:artistDictionary];
+        
+        if (artist) {
+            [artists addObject:artist];
+        } else {
+            // TODO: One of our "required" fields might be optional and we may need to debug this with real data
+            NSLog(@"Unable to parse artist dictionary: %@", artistDictionary);
+        }
+    }
+    
+    return [self initWithArtists:artists];
+}
 
 @end

fec1eed28430d2231e3a5972c711cb584525b1e8
David Wright <davidtwright89@gmail.com>
2020-06-01 02:03:16 -0400

    Added a FavoriteArtistsController class

---
+++ Favorite Artists/Favorite Artists/FavoriteArtistsController.h
@@ -0,0 +1,17 @@
+//
+//  FavoriteArtistsController.h
+//  Favorite Artists
+//
+//  Created by David Wright on 6/1/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FavoriteArtistsController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/FavoriteArtistsController.m
@@ -0,0 +1,13 @@
+//
+//  FavoriteArtistsController.m
+//  Favorite Artists
+//
+//  Created by David Wright on 6/1/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "FavoriteArtistsController.h"
+
+@implementation FavoriteArtistsController
+
+@end

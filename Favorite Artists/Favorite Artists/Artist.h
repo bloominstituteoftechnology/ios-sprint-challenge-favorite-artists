70823fe393434da81cf8c558c54ee8db61b0fc13
David Wright <davidtwright89@gmail.com>
2020-06-01 01:25:52 -0400

    Declared properties and initializers in Artist.h

--- Favorite Artists/Favorite Artists/Artist.h
+++ Favorite Artists/Favorite Artists/Artist.h
@@ -12,6 +12,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Artist : NSObject
 
+// Initializer
+- (instancetype)initWithName:(NSString *)name
+                  yearFormed:(int)yearFormed
+                   biography:(NSString *)biography NS_DESIGNATED_INITIALIZER;
+
+// Properties
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly) int yearFormed;
+@property (nonatomic, readonly, copy, nullable) NSString *biography;
+
 @end
 
 NS_ASSUME_NONNULL_END

d7e753bfbebe8bc98a015290a246fb3751ab7b28
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-31 20:57:59 -0400

    Created NSJSONSerialization category for model object and implemented persistence with NSDictionary

---
+++ FavoriteArtists/FavoriteArtists/SKIArtist+NSJSONSerialization.h
@@ -0,0 +1,16 @@
+//
+//  SKIArtist+NSJSONSerialization.h
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKIArtist_NSJSONSerialization : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/SKIArtist+NSJSONSerialization.m
@@ -0,0 +1,12 @@
+//
+//  SKIArtist+NSJSONSerialization.m
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import "SKIArtist+NSJSONSerialization.h"
+
+@implementation SKIArtist_NSJSONSerialization
+
+@end

57a28e367955655bc8211277e517b4395ce12468
David Wright <davidtwright89@gmail.com>
2020-06-01 01:29:10 -0400

    Implemented the initializer and toDictionary method in the NSJSONSerialization category

--- Favorite Artists/Favorite Artists/Artist+NSJSONSerialization.m
+++ Favorite Artists/Favorite Artists/Artist+NSJSONSerialization.m
@@ -10,4 +10,33 @@
 
 @implementation Artist (NSJSONSerialization)
 
+- (nullable instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSString *name = [dictionary objectForKey:@"strArtist"];
+    if (![name isKindOfClass:[NSString class]]) return nil;
+    
+    NSNumber *yearFormed = [dictionary objectForKey:@"intFormedYear"];
+    if ([yearFormed isKindOfClass:[NSNull class]]) {
+        yearFormed = nil;
+    } else if (![yearFormed isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSString *biography = [dictionary objectForKey:@"strBiographyEN"];
+    if ([biography isKindOfClass:[NSNull class]]) {
+        biography = nil;
+    } else if (![biography isKindOfClass:[NSString class]]) return nil;
+    
+    return [self initWithName:name
+                   yearFormed:yearFormed.intValue
+                    biography:biography];
+}
+
+- (NSDictionary *)toDictionary
+{
+    NSNumber *yearFormed = (self.yearFormed == 0) ? [NSNumber numberWithInt:self.yearFormed] : nil;
+    
+    return @{ @"strArtist": self.name,
+              @"intFormedYear": yearFormed,
+              @"strBiographyEN": self.biography };
+}
+
 @end

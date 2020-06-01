1f6c29a78dee0115a9cfc7ae39d74d7311711fd9
David Wright <davidtwright89@gmail.com>
2020-06-01 00:49:42 -0400

    Added an NSJSONSerialization category to the Artist model

---
+++ Favorite Artists/Favorite Artists/Artist+NSJSONSerialization.h
@@ -0,0 +1,18 @@
+//
+//  Artist+NSJSONSerialization.h
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Artist.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist (NSJSONSerialization)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/Artist+NSJSONSerialization.m
@@ -0,0 +1,13 @@
+//
+//  Artist+NSJSONSerialization.m
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "Artist+NSJSONSerialization.h"
+
+@implementation Artist (NSJSONSerialization)
+
+@end

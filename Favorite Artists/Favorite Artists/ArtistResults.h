10cd07ed3149da0a650142c35882e45fd0861b6a
David Wright <davidtwright89@gmail.com>
2020-06-01 00:47:14 -0400

    Added ArtistResults class

---
+++ Favorite Artists/Favorite Artists/ArtistResults.h
@@ -0,0 +1,17 @@
+//
+//  ArtistResults.h
+//  Favorite Artists
+//
+//  Created by David Wright on 6/1/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/ArtistResults.m
@@ -0,0 +1,13 @@
+//
+//  ArtistResults.m
+//  Favorite Artists
+//
+//  Created by David Wright on 6/1/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "ArtistResults.h"
+
+@implementation ArtistResults
+
+@end

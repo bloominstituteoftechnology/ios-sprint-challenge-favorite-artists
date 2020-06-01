0682b2b5b7199806ce048508f6e06dea98be5450
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-31 21:00:27 -0400

    Initial model controller setup

---
+++ FavoriteArtists/FavoriteArtists/SKIArtistController.h
@@ -0,0 +1,16 @@
+//
+//  SKIArtistController.h
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SKIArtistController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/SKIArtistController.m
@@ -0,0 +1,12 @@
+//
+//  SKIArtistController.m
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import "SKIArtistController.h"
+
+@implementation SKIArtistController
+
+@end

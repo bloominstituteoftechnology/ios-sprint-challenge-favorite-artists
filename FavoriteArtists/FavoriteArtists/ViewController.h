094f8a674c48aa67e00428644e7e133caff17603
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-31 20:32:10 -0400

    Setup Storyboard

---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Joshua Rutkowski on 5/31/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

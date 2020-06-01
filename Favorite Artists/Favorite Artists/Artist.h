a2b97e4538a9c993c6b561c85bb6525e1f55d3bb
David Wright <davidtwright89@gmail.com>
2020-05-31 23:36:16 -0400

    Added an Artist model class

---
+++ Favorite Artists/Favorite Artists/Artist.h
@@ -0,0 +1,17 @@
+//
+//  Artist.h
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artists/Favorite Artists/Artist.m
@@ -0,0 +1,13 @@
+//
+//  Artist.m
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end

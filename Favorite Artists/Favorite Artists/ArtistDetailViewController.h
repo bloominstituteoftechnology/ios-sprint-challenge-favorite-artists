57f24e82a71a2c8bc257e995fde1651c2c07085c
David Wright <davidtwright89@gmail.com>
2020-05-31 23:06:24 -0400

    Added an ArtistDetailViewController class

---
+++ Favorite Artists/Favorite Artists/ArtistDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ArtistDetailViewController.h
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

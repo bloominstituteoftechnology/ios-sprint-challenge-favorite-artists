35af2b4c218342ce430da1aa03e147558208318b
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-05-31 21:17:34 -0400

    setup numberOfRowsInSection to return count of saved artists

--- FavoriteArtists/FavoriteArtists/SKITableViewController.m
+++ FavoriteArtists/FavoriteArtists/SKITableViewController.m
@@ -37,25 +37,19 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.controller.savedArtists.count;
 }
 
-/*
+
  - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
- UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+ UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
  
  // Configure the cell...
  
  return cell;
  }
- */
+
 
 /*
  // Override to support conditional editing of the table view.

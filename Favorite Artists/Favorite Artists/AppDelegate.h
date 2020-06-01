95c925e4cef5cf565e2ee09e696e7a7d367db556
David Wright <davidtwright89@gmail.com>
2020-05-31 23:02:06 -0400

    Created a new Xcode project. Setup the UI in storyboards

---
+++ Favorite Artists/Favorite Artists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Favorite Artists
+//
+//  Created by David Wright on 5/31/20.
+//  Copyright © 2020 David Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+

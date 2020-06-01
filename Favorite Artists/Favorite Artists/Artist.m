aee26c2897f043254e085b86385e93ecc8f4db9c
David Wright <davidtwright89@gmail.com>
2020-06-01 01:26:16 -0400

    Implemented initializers in Artist.m

--- Favorite Artists/Favorite Artists/Artist.m
+++ Favorite Artists/Favorite Artists/Artist.m
@@ -10,4 +10,14 @@
 
 @implementation Artist
 
+- (instancetype)initWithName:(NSString *)name yearFormed:(int)yearFormed biography:(NSString *)biography
+{
+    if (self = [super init]) {
+        _name = name.copy;
+        _yearFormed = yearFormed;
+        _biography = biography.copy;
+    }
+    return self;
+}
+
 @end

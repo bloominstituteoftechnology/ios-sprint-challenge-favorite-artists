86cd49346fa8edc692d879aa4e6e6bec6a9aed3f
Alex Shillingford <alex.shillingford@icloud.com>
2020-06-11 14:49:52 -0600

    ViewController should now be up and running (still need to test once TableVC is working

--- FavoriteArtists_OBJC/FavoriteArtists_OBJC/ViewController.m
+++ FavoriteArtists_OBJC/FavoriteArtists_OBJC/ViewController.m
@@ -25,7 +25,48 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self.searchBar setDelegate:self];
+    [self updateViews];
 }
+
+- (void)updateViews {
+    
+    self.artistLabel.text = self.artist.name;
+    self.textView.text = self.artist.biography;
+    self.formedLabel.text = [self yearString];
+}
+
+- (NSString *)yearString {
+    if (self.artist.yearFormed != 0) {
+        return [NSString stringWithFormat:@"%i", self.artist.yearFormed];
+    } else {
+        return @"Not available";
+    }
+}
+
+- (IBAction)saveTapped:(id)sender {
+    if (self.artist) {
+        NSLog(@"saveTapped");
+        [self.artistController saveArtist:self.artist];
+        [self.navigationController popViewControllerAnimated:true];
+    }
+}
+
+
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSString *searchTerm = searchBar.text;
+    
+    [self.artistController searchForArtistWithName:searchTerm completion:^(WASArtist *artist, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Artist: %@, Error: %@", artist.name, error);
+            self.artist = artist;
+            [self updateViews];
+            NSLog(@"called!");
+        });
+    }];
+}
+
 
 
 @end

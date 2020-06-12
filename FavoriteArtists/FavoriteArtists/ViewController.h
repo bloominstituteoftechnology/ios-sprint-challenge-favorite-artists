644f73e3380003a8dc2083a3525a83f00fa80d44
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-12 08:27:43 -0700

    Added the starting project

---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Bhawnish Kumar on 6/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Bhawnish Kumar on 6/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

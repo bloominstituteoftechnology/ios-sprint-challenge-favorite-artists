61a99080891fdee8c72f84e0ca6d5a1735a54451
Chris Dobek <dobek.christopher@gmail.com>
2020-06-12 11:08:06 -0400

    FavoriteArtists project created

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Chris Dobek on 6/12/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Chris Dobek on 6/12/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

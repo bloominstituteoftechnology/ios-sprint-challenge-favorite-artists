cd6641c72e51a14fadc0f6eeacf3dc51fc6d184c
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-12 10:36:38 -0500

    Added stubs for table and detail view controllers

---
+++ FavoriteArtists/FavoriteArtists/View Controller/PDMArtistDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  PDMArtistDetailViewController.h
+//  FavoriteArtists
+//
+//  Created by Patrick Millet on 6/12/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PDMArtistDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/View Controller/PDMArtistDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  PDMArtistDetailViewController.m
+//  FavoriteArtists
+//
+//  Created by Patrick Millet on 6/12/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import "PDMArtistDetailViewController.h"
+
+@interface PDMArtistDetailViewController ()
+
+@end
+
+@implementation PDMArtistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

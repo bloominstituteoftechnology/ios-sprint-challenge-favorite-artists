502d19088ff655579ae428b2531bdaa9a848ff8e
Ezra Black <dev.ezrablack@gmail.com>
2020-06-12 11:23:12 -0500

    Base Project Implementation

---
+++ ArtistsObjCSprint/ArtistsObjCSprint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ArtistsObjCSprint
+//
+//  Created by Ezra Black on 6/12/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ArtistsObjCSprint/ArtistsObjCSprint/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ArtistsObjCSprint
+//
+//  Created by Ezra Black on 6/12/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

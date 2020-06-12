a9e0e39d6496e59be85823ed6d4709068c63ff5e
Shawn James <shawn.james@me.com>
2020-06-12 11:22:33 -0400

    Added model files

---
+++ FavoriteArtists/FavoriteArtists/Model/SDJArtist.m
@@ -0,0 +1,13 @@
+//
+//  SDJArtist.m
+//  FavoriteArtists
+//
+//  Created by Shawn James on 6/12/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import "SDJArtist.h"
+
+@implementation SDJArtist
+
+@end

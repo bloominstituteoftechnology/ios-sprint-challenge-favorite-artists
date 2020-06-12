50e7a6a179791c476541ffc18beaeafc93efcbdd
Shawn James <shawn.james@me.com>
2020-06-12 11:19:40 -0400

    Added ViewControllerFIles

---
+++ FavoriteArtists/FavoriteArtists/ViewControllers/DetailVC/SDJDetailVC.h
@@ -0,0 +1,17 @@
+//
+//  SDJDetailVC.h
+//  FavoriteArtists
+//
+//  Created by Shawn James on 6/12/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SDJDetailVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ViewControllers/HomeVC/SDJHomeVC.m
@@ -0,0 +1,32 @@
+//
+//  SDJHomeVC.m
+//  FavoriteArtists
+//
+//  Created by Shawn James on 6/12/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import "SDJHomeVC.h"
+
+@interface SDJHomeVC ()
+
+@end
+
+@implementation SDJHomeVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

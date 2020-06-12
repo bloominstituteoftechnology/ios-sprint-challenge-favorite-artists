abdb9d939240b7c1fadb56932fb628a2a1ce3680
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-12 10:13:23 -0500

    Created XCode Project

---
+++ FavoriteArtists/FavoriteArtists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Patrick Millet on 6/12/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Patrick Millet on 6/12/20.
+//  Copyright © 2020 PatrickMillet79. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

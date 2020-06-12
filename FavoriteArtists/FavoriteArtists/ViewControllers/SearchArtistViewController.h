bdb184677a16e528151422437d78fc9ad94dcf24
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-12 08:50:00 -0700

    Added some labels

---
+++ FavoriteArtists/FavoriteArtists/ViewControllers/SearchArtistViewController.h
@@ -0,0 +1,17 @@
+//
+//  SearchArtistViewController.h
+//  FavoriteArtists
+//
+//  Created by Bhawnish Kumar on 6/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchArtistViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ViewControllers/SearchArtistViewController.m
@@ -0,0 +1,32 @@
+//
+//  SearchArtistViewController.m
+//  FavoriteArtists
+//
+//  Created by Bhawnish Kumar on 6/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import "SearchArtistViewController.h"
+
+@interface SearchArtistViewController ()
+
+@end
+
+@implementation SearchArtistViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

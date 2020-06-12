d862c03f05c8855fc9d91a7a790f907b000386a8
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-06-12 08:58:30 -0700

    Added the .h model file implementation

---
+++ FavoriteArtists/FavoriteArtists/Models/LSIArtist.m
@@ -0,0 +1,13 @@
+//
+//  LSIArtist.m
+//  FavoriteArtists
+//
+//  Created by Bhawnish Kumar on 6/12/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+#import "LSIArtist.h"
+
+@implementation LSIArtist
+
+@end

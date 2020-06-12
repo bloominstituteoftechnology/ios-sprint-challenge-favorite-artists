bad3d53978db4c44bfd9e54b6aff7ebea70b8c8a
Ezra Black <dev.ezrablack@gmail.com>
2020-06-12 11:30:45 -0500

    Artsist Model Representation.

---
+++ ArtistsObjCSprint/ArtistsObjCSprint/CNSArtistModel.m
@@ -0,0 +1,15 @@
+//
+//  CNSArtistModel.m
+//  ArtistsObjCSprint
+//
+//  Created by Ezra Black on 6/12/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+#import "CNSArtistModel.h"
+
+@implementation CNSArtistModel
+
+
+
+@end

5488e938a6335fcaad657b3fa9aec62e25faa979
Mark Poggi <markp@mac.com>
2020-06-12 09:31:54 -0700

    Created model controller files.

---
+++ Artists/Artists/Model Controller/MJPArtistController.h
@@ -0,0 +1,17 @@
+//
+//  MJPAristController.h
+//  Artists
+//
+//  Created by Mark Poggi on 6/12/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MJPArtistController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Artists/Artists/Model Controller/MJPArtistController.m
@@ -0,0 +1,13 @@
+//
+//  MJPAristController.m
+//  Artists
+//
+//  Created by Mark Poggi on 6/12/20.
+//  Copyright © 2020 Mark Poggi. All rights reserved.
+//
+
+#import "MJPArtistController.h"
+
+@implementation MJPArtistController
+
+@end

64837ff1ea51ea0ffea6fceb4c50f35294d96140
Brad <braddiroff@icloud.com>
2020-06-12 11:18:03 -0400

    Created the project

---
+++ My Favorite Artist/My Favorite Artist/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  My Favorite Artist
+//
+//  Created by Bradley Diroff on 6/12/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ My Favorite Artist/My Favorite Artist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  My Favorite Artist
+//
+//  Created by Bradley Diroff on 6/12/20.
+//  Copyright © 2020 Bradley Diroff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

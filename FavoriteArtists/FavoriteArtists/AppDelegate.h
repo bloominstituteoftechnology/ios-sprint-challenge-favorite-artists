e3da036e3e469b819ff12867d85737efcbec863f
Shawn James <shawn.james@me.com>
2020-06-12 11:06:03 -0400

    Add starter project

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Shawn James on 6/12/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Shawn James on 6/12/20.
+//  Copyright © 2020 Shawn James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

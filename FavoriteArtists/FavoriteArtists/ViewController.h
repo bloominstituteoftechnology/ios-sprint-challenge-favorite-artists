4e45a1150a4a1532a1ebd497273da6713c877301
nonye <nnezekwo@gmail.com>
2020-07-17 08:32:45 -0700

    UI Complete

---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Nonye on 7/17/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Nonye on 7/17/20.
+//  Copyright © 2020 Nonye Ezekwo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

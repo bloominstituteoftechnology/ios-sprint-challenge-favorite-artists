05e861ada38770d199e7c256b33ea3d974c6927e
Cody Morley <codymorleyinc@gmail.com>
2020-07-17 08:51:16 -0700

    Golobal - Outline
    Added Remaining groups. Created model file CAMArtist (I think this officially puts me at the end of what I know how to do in this project).

---
+++ FavoriteArtists/FavoriteArtists/Models/CAMArtist.m
@@ -0,0 +1,13 @@
+//
+//  CAMArtist.m
+//  FavoriteArtists
+//
+//  Created by Cody Morley on 7/17/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import "CAMArtist.h"
+
+@implementation CAMArtist
+
+@end

cbcd54ac63d808036b4dd5a971b9f037f839d197
Cody Morley <codymorleyinc@gmail.com>
2020-07-17 08:24:35 -0700

    Outline
    Clean Outline to begin project.
    
    (I am in no way confident in this. The group projects from the week meant I never got to learn this on my own which is the only way I'm able to retain these things. I am flying about 80% blind in objective-C and I expect this will take the entire weekend.

---
+++ FavoriteArtists/FavoriteArtists/Application/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Bling Morley on 7/17/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

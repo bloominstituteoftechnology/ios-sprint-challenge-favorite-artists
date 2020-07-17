5a3fce9bad2490278790a33c7b21aaeaee275fa9
Stephanie <user.name>
2020-07-17 09:23:13 -0700

    storyboard set up

---
+++ FavoriteArtistsSprint/FavoriteArtistsSprint/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtistsSprint
+//
+//  Created by Stephanie Ballard on 7/17/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtistsSprint/FavoriteArtistsSprint/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtistsSprint
+//
+//  Created by Stephanie Ballard on 7/17/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

8b2396ee35129a1d23ac27a29b3a972ffb17a8e2
Cody Morley <codymorleyinc@gmail.com>
2020-07-17 16:22:51 -0700

    CAMArtistController.m
    finished save, load, and delete methods in model controller. moving on to search.

--- FavoriteArtists/FavoriteArtists/Model Controllers/CAMArtistController.m
+++ FavoriteArtists/FavoriteArtists/Model Controllers/CAMArtistController.m
@@ -17,9 +17,17 @@
 @end
 
 @implementation CAMArtistController
-
+//MARK: - Internal Properties & Get Methods-
 static NSString *baseURLString = @"https://www.theaudiodb.com/api/v1/json/1/search.php";
 
+- (NSArray *)savedArtists
+{
+    return [self.favoriteArtistsSafeStore copy];
+}
+
+
+
+//MARK: - Inits -
 - (instancetype)init
 {
     self = [super init];
@@ -29,9 +37,73 @@ static NSString *baseURLString = @"https://www.theaudiodb.com/api/v1/json/1/sear
     return self;
 }
 
-- (NSArray *)savedArtists {
-    return [self.favoriteArtistsSafeStore copy];
+
+
+//MARK: - Actions -
+- (void)loadArtists
+{
+    NSURL *url = [self favoritesURL];
+    NSDictionary *favoriteArtists = [NSDictionary dictionaryWithContentsOfURL: url];
+    if (![favoriteArtists[@"artists"] isEqual: @""]) {
+        NSArray *artists = favoriteArtists[@"artists"];
+        for (NSDictionary *dictionary in artists) {
+            CAMArtist *artist = [[CAMArtist alloc]initWithDictionary: dictionary];
+            [self.favoriteArtistsSafeStore addObject: artist];
+        }
+    }
 }
 
+- (void)saveArtist:(CAMArtist *)artist
+{
+    [self.favoriteArtistsSafeStore addObject: artist];
+    [self saveArtists];
+}
+
+- (void)deleteArtist:(CAMArtist *)artist
+{
+    [self.favoriteArtistsSafeStore removeObject:artist];
+    [self saveArtists];
+}
+
+-(void)searchForName:(NSString *)name
+{
+    
+}
+
+
+//MARK: - Methods -
+- (NSURL *)favoritesURL
+{
+    NSURL *documentDirectory = [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory
+                                                                       inDomains:NSUserDomainMask] firstObject];
+    NSString *fileName = @"favoriteArtists.json";
+    return [documentDirectory URLByAppendingPathComponent:fileName];
+}
+
+- (void)saveArtists
+{
+    NSURL *url = [self favoritesURL];
+    NSError *error = nil;
+    NSMutableArray *artists = [[NSMutableArray alloc] init];
+    
+    for (CAMArtist *artist in self.favoriteArtistsSafeStore) {
+        NSDictionary *artistData = [artist createDictionary];
+        [artists addObject: artistData];
+    }
+    
+    NSDictionary *artistsToSave =
+    @{
+        @"artists" : artists
+    };
+    
+    bool success = [artistsToSave writeToURL:url error:nil];
+    
+    if (success) {
+        return;
+    } else {
+        NSLog(@"Error saving favorite artists. Here's what happened: %@", error);
+    }
+    
+}
 
 @end

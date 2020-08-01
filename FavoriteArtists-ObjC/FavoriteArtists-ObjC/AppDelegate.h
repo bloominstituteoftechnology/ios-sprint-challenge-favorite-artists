4b49a22d11aff0c95af7d1593f607c5438bd1e86
Kevin Stewart <kistewart419@gmail.com>
2020-07-31 21:20:23 -0400

    Set up the storyboard

---
+++ FavoriteArtists-ObjC/FavoriteArtists-ObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists-ObjC
+//
+//  Created by Kevin Stewart on 7/31/20.
+//  Copyright © 2020 Kevin Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+

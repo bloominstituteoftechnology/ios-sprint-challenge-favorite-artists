d661a910ad080df5b2d99110bd0527cc6832ee7f
Chad Parker <chad@chadparker.com>
2020-07-31 18:49:35 -0700

    Add Artist model object

---
+++ FavoriteArtists/FavoriteArtists/CLPArtist.m
@@ -0,0 +1,13 @@
+//
+//  CLPArtist.m
+//  FavoriteArtists
+//
+//  Created by Chad Parker on 7/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "CLPArtist.h"
+
+@implementation CLPArtist
+
+@end

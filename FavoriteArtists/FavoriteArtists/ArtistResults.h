e58ed3a8b9ea647693d8c2c04380babc085a7ac1
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 18:25:54 -0700

    Added Artiest Results Class

---
+++ FavoriteArtists/FavoriteArtists/ArtistResults.h
@@ -0,0 +1,17 @@
+//
+//  ArtistResults.h
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistResults : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistResults.m
@@ -0,0 +1,13 @@
+//
+//  ArtistResults.m
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "ArtistResults.h"
+
+@implementation ArtistResults
+
+@end

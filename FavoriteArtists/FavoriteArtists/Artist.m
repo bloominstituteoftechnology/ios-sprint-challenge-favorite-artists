00afa0d46751a72a06fa63c0306d3c9a28aca39e
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 19:59:54 -0700

    Set up detail view controller with minor errors when editing

--- FavoriteArtists/FavoriteArtists/Artist.m
+++ FavoriteArtists/FavoriteArtists/Artist.m
@@ -37,7 +37,10 @@
 
     NSNumber *yearFormed = [dictionary objectForKey:@"intFormedYear"];
     if ([yearFormed isKindOfClass:[NSNull class]]) yearFormed = nil;
-    else if (![yearFormed isKindOfClass:[NSNumber class]]) return nil;
+    else if ([yearFormed isKindOfClass:[NSString class]]) {
+    NSString *yearFormedString = [dictionary objectForKey:@"intFormedYear"];
+    yearFormed = @([yearFormedString intValue]);
+    } else if (![yearFormed isKindOfClass:[NSNumber class]]) return nil;
 
     NSString *biography = [dictionary objectForKey:@"strBiographyEN"];
     if ([biography isKindOfClass:[NSNull class]]) biography = nil;
--- FavoriteArtists/FavoriteArtists/ArtistDetailViewController.m
+++ FavoriteArtists/FavoriteArtists/ArtistDetailViewController.m
@@ -7,17 +7,85 @@
 //
 
 #import "ArtistDetailViewController.h"
+#import "Artist.h"
+#import "ArtistFetcher.h"
+#import "ArtistsController.h"
 
-@interface ArtistDetailViewController ()
+@interface ArtistDetailViewController () <UISearchBarDelegate>
+
+// Properties
+@property ArtistFetcher *artistFetcher;
+
+// IBOutlets
+@property (nonatomic) IBOutlet UIBarButtonItem *saveButton;
+@property (nonatomic) IBOutlet UISearchBar *searchBar;
+@property (nonatomic) IBOutlet UILabel *artistNameLabel;
+@property (nonatomic) IBOutlet UILabel *yearFormedLabel;
+@property (nonatomic) IBOutlet UILabel *biographyLabel;
 
 @end
+
 
 @implementation ArtistDetailViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.artistFetcher = [[ArtistFetcher alloc] init];
+
+    self.searchBar.delegate = self;
+
+    if (self.isShowingArtistDetail) {
+        [self.searchBar removeFromSuperview];
+        self.navigationItem.rightBarButtonItem = nil;
+    }
+
+    [self updateViews];
 }
 
+- (IBAction)saveButtonTapped:(UIBarButtonItem *)sender
+{
+    if (self.artist == nil) return;
+    [self.artistController addArtist:self.artist];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)updateViews
+{
+    if (self.artist != nil) {
+
+        self.artistNameLabel.text = self.artist.name;
+        self.biographyLabel.text = self.artist.biography;
+
+        if (self.artist.yearFormed != 0) {
+            NSString *yearFormedString = [NSString stringWithFormat:@"Formed in %d", self.artist.yearFormed];
+            self.yearFormedLabel.text = yearFormedString;
+        } else {
+            self.yearFormedLabel.text = @"Year Formed: Unkown";
+        }
+    } else {
+        self.artistNameLabel.text = nil;
+        self.biographyLabel.text = nil;
+        self.yearFormedLabel.text = nil;
+    }
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    NSString *searchTerm = searchBar.text;
+    if ((searchTerm == nil) | [searchTerm isEqualToString:@""]) return;
+
+    NSLog(@"Searching for %@", searchTerm);
+
+    [self.artistFetcher fetchArtistsWithName:searchTerm completionHandler:^(NSArray * _Nullable artists, NSError * _Nullable error) {
+        NSLog(@"Found %ld results!", artists.count);
+
+        if (artists.count > 0) {
+            self.artist = artists[0];
+        }
+
+        [self updateViews];
+    }];
+}
 
 @end

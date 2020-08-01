b47bd19acab63b2b1fa223e48c387d4817e1d1dc
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 18:23:40 -0700

    Added Artist Fetcher class

---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.h
@@ -0,0 +1,17 @@
+//
+//  ArtistFetcher.h
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.m
@@ -0,0 +1,13 @@
+//
+//  ArtistFetcher.m
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "ArtistFetcher.h"
+
+@implementation ArtistFetcher
+
+@end

ef124fdeb43b4a94768077f2b5f35d8148934cc0
Chad Parker <chad@chadparker.com>
2020-07-31 18:52:06 -0700

    Add NSJSONSerialization category on Artist

---
+++ FavoriteArtists/FavoriteArtists/CLPArtist+NSJSONSerialization.h
@@ -0,0 +1,17 @@
+//
+//  CLPArtist+NSJSONSerialization.h
+//  FavoriteArtists
+//
+//  Created by Chad Parker on 7/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "CLPArtist.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLPArtist (NSJSONSerialization)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/CLPArtist+NSJSONSerialization.m
@@ -0,0 +1,13 @@
+//
+//  CLPArtist+NSJSONSerialization.m
+//  FavoriteArtists
+//
+//  Created by Chad Parker on 7/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "CLPArtist+NSJSONSerialization.h"
+
+@implementation CLPArtist (NSJSONSerialization)
+
+@end

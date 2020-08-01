5be46420d9cdd6f99444a559946509b0b5bb5640
Chad Parker <chad@chadparker.com>
2020-07-31 19:16:05 -0700

    Add ArtistController

---
+++ FavoriteArtists/FavoriteArtists/Model/CLPArtistController.h
@@ -0,0 +1,17 @@
+//
+//  CLPArtistController.h
+//  FavoriteArtists
+//
+//  Created by Chad Parker on 7/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CLPArtistController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Model/CLPArtistController.m
@@ -0,0 +1,13 @@
+//
+//  CLPArtistController.m
+//  FavoriteArtists
+//
+//  Created by Chad Parker on 7/31/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+#import "CLPArtistController.h"
+
+@implementation CLPArtistController
+
+@end

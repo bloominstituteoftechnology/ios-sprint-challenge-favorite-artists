134730f17b9951a9c849f9344a66c3a0a8d8efc4
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 18:21:54 -0700

    Added ArtistTableViewController Class

---
+++ FavoriteArtists/FavoriteArtists/ArtistsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ArtistsTableViewController.h
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistsTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

10fff33901b0144c2ae95e14cb562fcbb5ecf16e
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 19:14:22 -0700

    Added ArtistController Class

---
+++ FavoriteArtists/FavoriteArtists/ArtistsController.h
@@ -0,0 +1,17 @@
+//
+//  ArtistsController.h
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistsController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistsController.m
@@ -0,0 +1,13 @@
+//
+//  ArtistsController.m
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "ArtistsController.h"
+
+@implementation ArtistsController
+
+@end

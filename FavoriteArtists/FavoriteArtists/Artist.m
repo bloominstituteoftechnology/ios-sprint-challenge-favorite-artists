4cf57a1f8d885c19d195ce51885372aee9265c2b
Claudia Maciel <clc_80@yahoo.com>
2020-07-31 18:25:11 -0700

    Added Artist Model Class

---
+++ FavoriteArtists/FavoriteArtists/Artist.m
@@ -0,0 +1,13 @@
+//
+//  Artist.m
+//  FavoriteArtists
+//
+//  Created by Claudia Maciel on 7/31/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end

0672fe15c311bddd0a95f5c8a8c10ef89d27d011
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-02 21:55:43 -0400

    Added artist fetcher files

---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.h
@@ -0,0 +1,17 @@
+//
+//  ArtistFetcher.h
+//  FavoriteArtists
+//
+//  Created by Sean Acres on 8/2/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.m
@@ -0,0 +1,13 @@
+//
+//  ArtistFetcher.m
+//  FavoriteArtists
+//
+//  Created by Sean Acres on 8/2/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "ArtistFetcher.h"
+
+@implementation ArtistFetcher
+
+@end

13b853f1d0b4a434a9ee4ee06208006f2ad7fdae
Matt Martindale <martindalematt18@yahoo.com>
2020-08-02 19:31:50 -0600

    Created artist results object

--- FavoriteArtists/FavoriteArtists/ArtistResults.h
+++ FavoriteArtists/FavoriteArtists/ArtistResults.h
@@ -14,6 +14,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface ArtistResults : NSObject
 
+- (instancetype)initWithArtists: (NSArray<Artist *> *)artists NS_DESIGNATED_INITIALIZER;
+- (nullable instancetype)initWithDictionary: (NSDictionary *)dictionary;
+
+@property (nonatomic, readonly, copy) NSArray<Artist *> *artists;
 
 @end
 
--- FavoriteArtists/FavoriteArtists/ArtistResults.m
+++ FavoriteArtists/FavoriteArtists/ArtistResults.m
@@ -7,7 +7,38 @@
 //
 
 #import "ArtistResults.h"
+#import "Artist.h"
+#import "Artist+NJSONSerialization.h"
 
 @implementation ArtistResults
+
+- (instancetype)initWithArtists:(NSArray<Artist *> *)artists
+{
+    if (self = [super init]) {
+        _artists = artists;
+    }
+    return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    NSArray *artistDictionaries = [dictionary objectForKey:@"artists"];
+    if (![artistDictionaries isKindOfClass:NSArray.class]) return nil;
+    
+    NSMutableArray *artists = [[NSMutableArray alloc] initWithCapacity:artistDictionaries.count];
+    
+    for (NSDictionary *artistDictionary in artistDictionaries) {
+        if (![artistDictionary isKindOfClass:NSDictionary.class]) continue;
+        
+        Artist *artist = [[Artist alloc] initWithDictionary:artistDictionary];
+        
+        if (artist) {
+            [artists addObject:artist];
+        } else {
+            NSLog(@"Unable to parse artist dictionary: %@", artistDictionary);
+        }
+    }
+    return [self initWithArtists:artists];
+}
 
 @end

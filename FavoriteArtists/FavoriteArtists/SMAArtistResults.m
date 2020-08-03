215dd369fc1b3d875c73921d1b993ee9cdd33e0f
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-02 21:45:55 -0400

    Added artist results file (api returns array)

---
+++ FavoriteArtists/FavoriteArtists/SMAArtistResults.m
@@ -0,0 +1,13 @@
+//
+//  SMAArtistResults.m
+//  FavoriteArtists
+//
+//  Created by Sean Acres on 8/2/20.
+//  Copyright © 2020 Sean Acres. All rights reserved.
+//
+
+#import "SMAArtistResults.h"
+
+@implementation SMAArtistResults
+
+@end

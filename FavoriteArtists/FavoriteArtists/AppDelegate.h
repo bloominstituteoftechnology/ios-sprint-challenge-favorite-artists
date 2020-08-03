0993a2271e8189221a2fbdc46c3b0ba644efd028
Kelson Hartle <kelson99hartle@icloud.com>
2020-08-02 19:10:38 -0600

    added xcode project to github repo

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Kelson Hartle on 8/2/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FavoriteArtists
+//
+//  Created by Kelson Hartle on 8/2/20.
+//  Copyright © 2020 Kelson Hartle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+

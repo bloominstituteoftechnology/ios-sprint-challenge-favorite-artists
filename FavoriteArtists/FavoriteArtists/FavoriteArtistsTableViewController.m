08bb35291cb36685051b25ffcfe4e66998c9c384
seanacres <6416690+seanacres@users.noreply.github.com>
2020-08-05 13:18:36 -0400

    Started setting up table view to display saved artists

--- FavoriteArtists/FavoriteArtists/FavoriteArtistsTableViewController.m
+++ FavoriteArtists/FavoriteArtists/FavoriteArtistsTableViewController.m
@@ -7,10 +7,12 @@
 //
 
 #import "FavoriteArtistsTableViewController.h"
+#import "SMAFavoriteArtist.h"
+#import "SMAFavoriteArtistController.h"
 
 @interface FavoriteArtistsTableViewController ()
 
-
+@property SMAFavoriteArtistController *favoriteArtistController;
 
 @end
 
@@ -19,11 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.favoriteArtistController = [[SMAFavoriteArtistController alloc] init];
 }
 
 - (IBAction)addNewArtistTapped:(id)sender {
@@ -32,8 +30,7 @@
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.favoriteArtistController.artists.count;
 }
 
 /*

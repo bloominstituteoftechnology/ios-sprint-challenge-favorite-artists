b1435dcdc4cc521905e52c1644850165ce9e33b7
Gerardo <gahernandez02@gmail.com>
2020-08-08 11:13:24 -0700

    Added ArtistDetailViewController file

---
+++ Favorite Artist/Favorite Artist/ArtistDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  ArtistDetailViewController.h
+//  Favorite Artist
+//
+//  Created by Gerardo Hernandez on 8/8/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Favorite Artist/Favorite Artist/ArtistDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  ArtistDetailViewController.m
+//  Favorite Artist
+//
+//  Created by Gerardo Hernandez on 8/8/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+#import "ArtistDetailViewController.h"
+
+@interface ArtistDetailViewController ()
+
+@end
+
+@implementation ArtistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

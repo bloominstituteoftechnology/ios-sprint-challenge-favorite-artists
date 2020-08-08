2e8cee15dfb3c6abfd02acfdc2a08de4e9dce8ab
Gerardo <gahernandez02@gmail.com>
2020-08-08 11:03:17 -0700

    Created an initializer to create the Artist model from a NSDictionary

---
+++ Favorite Artist/Favorite Artist/Artist+NSJSONSerialization.m
@@ -0,0 +1,46 @@
+//
+//  Artist+NSJSONSerialization.m
+//  Favorite Artist
+//
+//  Created by Gerardo Hernandez on 8/8/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+#import "Artist+NSJSONSerialization.h"
+
+
+@implementation Artist (NSJSONSerialization)
+
+- (instancetype)initwithDictionary:(NSDictionary *)dictionary
+{
+    NSString *name = [dictionary objectForKey:@"strArtist"];
+    if (![name isKindOfClass:[NSString class]]) return nil;
+    
+    NSNumber *yearFormed = [dictionary objectForKey:@"initFormedYear"];
+    if ([yearFormed isKindOfClass:[NSNull class]]) {
+        yearFormed = nil;
+    } else if ([yearFormed isKindOfClass:[NSString class]]) {
+        NSString *yearFormedString = [dictionary objectForKey:@"initFormedYear"];
+        yearFormed = @([yearFormedString intValue]);
+    } else if (![yearFormed isKindOfClass:[NSNumber class]]) return nil;
+    
+    NSString *biography = [dictionary objectForKey:@"strBiographyEN"];
+    if ([biography isKindOfClass:[NSNull class]]) {
+        biography = nil;
+    } else if (![biography isKindOfClass:[NSString class]]) return nil;
+    
+    return [self initWithName:name
+                   yearFormed:yearFormed.intValue
+                    biography:biography];
+}
+
+- (NSDictionary *)toDictionary
+{
+    NSNumber *yearFormed = [NSNumber numberWithInt:self.yearFormed];
+    
+    return @{ @"strArtist": self.name,
+              @"intFormedYear": yearFormed,
+              @"strBiographyEN": self.biography };
+}
+
+@end

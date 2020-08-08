4c89944b1215f64f1e541d9e90a0e36f50c76704
Gerardo <gahernandez02@gmail.com>
2020-08-08 12:05:05 -0700

    Declared initializers and artist property in ArtistFetchResults

--- Favorite Artist/Favorite Artist/FavoriteArtistController.m
+++ Favorite Artist/Favorite Artist/FavoriteArtistController.m
@@ -6,10 +6,82 @@
 //  Copyright © 2020 Gerardo Hernandez. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import "FavoriteArtistController.h"
+#import "Artist.h"
+#import "Artist+NSJSONSerialization.h"
 
 
+@interface FavoriteArtistController () {
+    NSMutableArray *_interalArtists;
+}
+
+@end
 
 @implementation FavoriteArtistController
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _interalArtists = [[NSMutableArray alloc] init];
+        
+        NSString *filePath = self.getFilePath;
+        NSFileManager *fileManger = [NSFileManager defaultManager];
+        
+        if ([fileManger fileExistsAtPath:filePath]) {
+            NSDictionary *dictionary = [[NSDictionary alloc] initWithContentsOfURL:self.getFileURL];
+            [self updateArtistArrayWithDictionary:dictionary];
+        }
+        
+    }
+    return self;
+}
+
+// Mark: - Convenience Accessors
+
+- (NSArray<Artist *> *)artist
+{
+    return _interalArtists.copy;
+}
+
+- (void)addArtist:(Artist *)artist
+{
+    [_interalArtists addObject:artist];
+   
+}
+
+- (NSURL *)getFileURL
+{
+    NSString *fileName = @"FavoriteArtistsData.plist";
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    NSURL *fileURL = [baseURL URLByAppendingPathComponent:fileName];
+    return fileURL;
+}
+
+- (NSString *)getFilePath
+{
+    return self.getFileURL.path;
+}
+
+- (void)saveToPersistentStore
+{
+    NSMutableDictionary *dictionary = [[NSMutableDictionary alloc] init];
+    for (Artist *artist in _interalArtists) {
+        NSString *key = artist.name;
+        dictionary[key] = artist.toDictionary;
+    }
+
+    [dictionary writeToURL:self.getFileURL atomically:YES];
+}
+
+- (void)updateArtistArrayWithDictionary:(NSDictionary *)dictionary
+{
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for (NSDictionary *artistDictionary in dictionary.allValues) {
+        Artist *artist = [[Artist alloc] initwithDictionary:artistDictionary];
+        [array addObject:artist];
+    }
+}
 @end

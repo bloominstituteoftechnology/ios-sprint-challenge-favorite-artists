f644e3be91c4dc69a60cc6be3d86006519b6ba7d
Gerardo <gahernandez02@gmail.com>
2020-08-08 12:32:02 -0700

    Declared and implemented ArtistFetcher class. Handled url errors

--- Favorite Artist/Favorite Artist/ArtistFetcher.m
+++ Favorite Artist/Favorite Artist/ArtistFetcher.m
@@ -7,10 +7,62 @@
 //
 
 #import "ArtistFetcher.h"
+#import "Artist.h"
+#import "ArtistFetchResults.h"
 
 static NSString *const ArtistFetcherBaseURLString = @"https://www.theaudiodb.com/api/v1/json/1/search.php?";
 
 
 @implementation ArtistFetcher
+
+- (void)fetchArtistsWithName:(NSString *)name completionHandler:(ArtistFetcherCompletionHandler)completionHandler
+{
+    NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithString:ArtistFetcherBaseURLString];
+    
+    urlComponents.queryItems= @[ [NSURLQueryItem queryItemWithName:@"s" value:name] ];
+     
+     NSURL *url = urlComponents.URL;
+     NSLog(@"Fetching Artist: %@", url);
+     
+      [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+             if (error) {
+                 NSLog(@"Error fetching artists: %@", error);
+                 
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     completionHandler(nil, error);
+                 });
+                 
+                 return;
+             }
+             
+             NSError *jsonError;
+             NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+             if (!dictionary) {
+                 NSLog(@"Error decoding JSON: %@", jsonError);
+                 
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     completionHandler(nil, jsonError);
+                 });
+                 
+                 return;
+             }
+             
+             ArtistFetchResults *results = [[ArtistFetchResults alloc] initWithDictionary:dictionary];
+             if (!results) {
+                 NSError *error = [NSError errorWithDomain:@"ArtistFetcherDomain" code:-1 userInfo:nil];
+                 
+                 dispatch_async(dispatch_get_main_queue(), ^{
+                     completionHandler(nil, error);
+                 });
+                 
+                 return;
+             }
+             
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 completionHandler(results.artist, nil);
+             });
+             
+         }] resume];
+     }
 
 @end

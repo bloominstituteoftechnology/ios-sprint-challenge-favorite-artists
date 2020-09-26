749aacac6ff56fb8d54ae116ad53a3a3c001636f
James McDougall <jameswiliammcdougall@gmail.com>
2020-09-25 20:40:16 -0500

    Initial Project commit

---
+++ FavoriteArtists/FavoriteArtists/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FavoriteArtists
+//
+//  Created by Lambda_School_loaner_226 on 9/25/20.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}

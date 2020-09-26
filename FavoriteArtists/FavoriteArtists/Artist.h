d6758b3c223f0b655a55d3d4843170fc2c1845c6
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-25 18:21:22 -0700

    Added Artist Model

---
+++ FavoriteArtists/FavoriteArtists/Artist.h
@@ -0,0 +1,18 @@
+//
+//  Artist.h
+//  FavoriteArtists
+//
+//  Created by Clayton Watkins on 9/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist : NSObject
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/Artist.m
@@ -0,0 +1,12 @@
+//
+//  Artist.m
+//  FavoriteArtists
+//
+//  Created by Clayton Watkins on 9/25/20.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end

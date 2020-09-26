c0ce3e008720c4ae00f12fbc4d9e7afde39f88ec
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-25 19:20:11 -0700

    Created fetch request in ArtistFetcher.m

--- FavoriteArtists/FavoriteArtists/ArtistFetcher.m
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.m
@@ -13,7 +13,52 @@ static NSString *const ArtistFetcherBaseURLString = @"https://www.theaudiodb.com
 
 @implementation ArtistFetcher
 
-
-
+- (void)fetchArtistWithName:(NSString *)artistName completionHandler:(ArtistFetcherCompletionHandler)completionHandler
+{
+    NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithString: ArtistFetcherBaseURLString];
+    
+    urlComponents.queryItems = @[ [NSURLQueryItem queryItemWithName:@"s" value:artistName] ];
+    
+    NSURL *url = urlComponents.URL;
+    NSLog(@"Fetching Aritists: %@", url);
+    
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error fetching artist: %@", error);
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler(nil, error);
+            });
+        }
+        
+        NSError *jsonError;
+        NSDictionary *dictionary = [NSJSONSerialization JSONObjectWithData:data
+                                                                   options:0
+                                                                     error:&jsonError];
+        if (!dictionary) {
+            NSLog(@"Error decoding JSON: %@", jsonError);
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler(nil, error);
+            });
+            return;
+        }
+        
+        ArtistResults *results = [[ArtistResults alloc]initWithDictionary:dictionary];
+        if (!results) {
+            NSError *error = [NSError errorWithDomain:@"ArtistFetcherDomain" code:-1 userInfo:nil];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completionHandler(nil, error);
+            });
+            return;
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            completionHandler(results.artists, nil);
+        });
+        
+    }] resume];
+}
 
 @end

3d8871aa296f1f5410ab6e92f146c2065672c863
Clayton Watkins <watkinclayton17@yahoo.com>
2020-09-25 18:57:38 -0700

    Created ArtistFetcher file

---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.h
@@ -0,0 +1,16 @@
+//
+//  ArtistFetcher.h
+//  FavoriteArtists
+//
+//  Created by Clayton Watkins on 9/25/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistFetcher : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistFetcher.m
@@ -0,0 +1,12 @@
+//
+//  ArtistFetcher.m
+//  FavoriteArtists
+//
+//  Created by Clayton Watkins on 9/25/20.
+//
+
+#import "ArtistFetcher.h"
+
+@implementation ArtistFetcher
+
+@end

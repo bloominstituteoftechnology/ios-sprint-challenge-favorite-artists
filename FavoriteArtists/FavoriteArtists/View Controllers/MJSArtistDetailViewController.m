274bb1af220620ee96825a7b501cf33483c0e961
morgan smith <mssmitty07@gmail.com>
2020-09-27 20:04:47 -0400

    finished storyboard and controllers

--- FavoriteArtists/FavoriteArtists/View Controllers/MJSArtistDetailViewController.m
+++ FavoriteArtists/FavoriteArtists/View Controllers/MJSArtistDetailViewController.m
@@ -11,7 +11,14 @@
 #import "MJSArtist+NSJSONSerialization.h"
 
 @interface MJSArtistDetailViewController ()<UISearchBarDelegate>
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UILabel *artistLabel;
+@property (weak, nonatomic) IBOutlet UILabel *yearFormed;
+@property (weak, nonatomic) IBOutlet UITextView *biographyTextView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *saveButton;
 
+@property (nonatomic) MJSArtist *artistSearch;
+@property (nonatomic) BOOL isArtist;
 
 @end
 
@@ -19,17 +26,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.searchBar.delegate = self;
+
+    if (self.artist == nil) {
+        self.isArtist = NO;
+        self.artistLabel.hidden = YES;
+        self.yearFormed.hidden = YES;
+        self.biographyTextView.hidden = YES;
+    } else {
+        self.isArtist = YES;
+        [self updateViews];
+    }
+
 }
 
-/*
-#pragma mark - Navigation
+- (void)updateViews {
+    if (self.isArtist == YES) {
+        self.searchBar.hidden = YES;
+        self.title = self.artist.artist;
+        self.artistLabel.hidden = YES;
+        self.yearFormed.hidden = NO;
+        self.biographyTextView.hidden = NO;
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+        if (self.artist.yearFormed == 0) {
+            self.yearFormed.text = @"N/A";
+        } else {
+            self.yearFormed.text = [NSString stringWithFormat:@"Formed in %d", self.artist.yearFormed];
+        }
+        self.biographyTextView.text = self.artist.biography;
+    } else {
+        self.artistLabel.hidden = NO;
+        self.yearFormed.hidden = NO;
+        self.biographyTextView.hidden = NO;
+
+        self.artistLabel.text = self.artistSearch.artist;
+
+        if (self.artistSearch.yearFormed == 0) {
+            self.yearFormed.text = @"N/A";
+        } else {
+            self.yearFormed.text = [NSString stringWithFormat:@"Formed in %d", self.artistSearch.yearFormed];
+        }
+
+        self.biographyTextView.text = self.artistSearch.biography;
+    }
 }
-*/
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    [self.artistController fetchArtistsWithName:searchBar.text completion:^(MJSArtist * _Nullable artist, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"Error fetching artist!");
+        }
+
+        NSLog(@"Fetched artist: %@", artist);
+        self.artistSearch = artist;
+
+        dispatch_async(dispatch_get_main_queue(), ^ {
+            [self updateViews];
+        });
+    }];
+}
+
+- (IBAction)saveButtonTapped:(UIBarButtonItem *)sender {
+    if (self.artistSearch == nil) {
+        return;
+    }
+
+    NSData *data = [NSJSONSerialization dataWithJSONObject:[self.artistSearch toDictionary] options:0 error:nil];
+    NSURL *directory = [[NSFileManager defaultManager] URLForDirectory: NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:nil];
+
+    NSURL *url = [[directory URLByAppendingPathComponent:self.artistSearch.artist] URLByAppendingPathExtension:@"json"];
+
+    NSLog(@"Directory: %@", directory);
+    NSLog(@"UR: %@", url);
+
+    [data writeToURL:url atomically:YES];
+    [self.navigationController popToRootViewControllerAnimated:true];
+}
 
 @end
--- FavoriteArtists/FavoriteArtists/View Controllers/MJSArtistTableViewController.m
+++ FavoriteArtists/FavoriteArtists/View Controllers/MJSArtistTableViewController.m
@@ -6,8 +6,14 @@
 //
 
 #import "MJSArtistTableViewController.h"
+#import "MJSArtist.h"
+#import "MJSArtistController.h"
+#import "MJSArtistDetailViewController.h"
 
 @interface MJSArtistTableViewController ()
+
+@property (nonatomic) MJSArtistController *artistController;
+@property (nonatomic) NSMutableArray *savedArtist;
 
 @end
 
@@ -15,35 +21,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+    NSArray *tempArtists = [self.artistController fetchSavedArtists];
+    self.savedArtist = [[NSMutableArray alloc] initWithArray:tempArtists];
+  
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    NSArray *tempArtist = [self.artistController fetchSavedArtists];
+    self.savedArtist = [[NSMutableArray alloc] initWithArray:tempArtist];
+
+    [self.tableView reloadData];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Incomplete implementation, return the number of sections
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 0;
+    return self.savedArtist.count;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ArtistCell" forIndexPath:indexPath];
+
+    MJSArtist *artist = self.savedArtist[indexPath.row];
+
+    if (artist.yearFormed == 0) {
+        cell.detailTextLabel.text = @"N/A";
+        cell.textLabel.text = artist.artist;
+    } else {
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"Formed in %d", artist.yearFormed];
+        cell.textLabel.text = artist.artist;
+    }
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
@@ -79,14 +100,31 @@
 }
 */
 
-/*
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([segue.identifier isEqualToString:@"AddArtist"]) {
+        MJSArtistDetailViewController *controller = (MJSArtistDetailViewController *)segue.destinationViewController;
+        controller.artistController = self.artistController;
+
+    }
+
+    if ([segue.identifier isEqualToString:@"ShowArtist"]) {
+        MJSArtistDetailViewController *controller = (MJSArtistDetailViewController *)segue.destinationViewController;
+        NSIndexPath *selectedIndexPath = [self.tableView indexPathForSelectedRow];
+        controller.artist = self.savedArtist[selectedIndexPath.row];
+    }
 }
-*/
+
+    - (MJSArtistController *)artistController {
+        if (!_artistController) {
+            _artistController = [[MJSArtistController alloc] init];
+        }
+
+        return _artistController;
+    }
+
 
 @end

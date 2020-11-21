6d10c7f33fb2e6e8362d95fd9383b5d5536e2e14
Cora Jacobson <cora-jacobson@lambdastudents.com>
2020-11-21 12:01:39 -0800

    Networking

---
+++ FavoriteArtists/FavoriteArtists/ArtistController.h
@@ -0,0 +1,16 @@
+//
+//  ArtistController.h
+//  FavoriteArtists
+//
+//  Created by Cora Jacobson on 11/21/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ArtistController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FavoriteArtists/FavoriteArtists/ArtistController.m
@@ -0,0 +1,12 @@
+//
+//  ArtistController.m
+//  FavoriteArtists
+//
+//  Created by Cora Jacobson on 11/21/20.
+//
+
+#import "ArtistController.h"
+
+@implementation ArtistController
+
+@end

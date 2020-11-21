c0670f4a8ce5e0cc0e8f551b773923430b03b5d4
brys <brys1saclaus2@gmail.com>
2020-11-21 10:23:31 -0800

    // Created Artist model file

---
+++ ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/Artist.h
@@ -0,0 +1,16 @@
+//
+//  Artist.h
+//  ios-FavoriteArtists-ObjectiveC-SprintChallenge
+//
+//  Created by BrysonSaclausa on 11/21/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Artist : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/Artist.m
@@ -0,0 +1,12 @@
+//
+//  Artist.m
+//  ios-FavoriteArtists-ObjectiveC-SprintChallenge
+//
+//  Created by BrysonSaclausa on 11/21/20.
+//
+
+#import "Artist.h"
+
+@implementation Artist
+
+@end

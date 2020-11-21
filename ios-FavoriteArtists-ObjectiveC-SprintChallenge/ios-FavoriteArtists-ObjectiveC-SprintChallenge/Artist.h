9351e364c53a497eafc021341a9a31bea552d572
brys <brys1saclaus2@gmail.com>
2020-11-21 12:15:19 -0800

    Data modeling work.

--- ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/Artist.h
+++ ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/Artist.h
@@ -11,6 +11,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Artist : NSObject
 
+@property (nonatomic, readonly, copy) NSString *artistName;
+@property (nonatomic, readonly, copy) NSString *artistBio;
+@property (nonatomic, readonly) int yearFormed;
+
+- (instancetype)initWithArtistName:(NSString *)aName
+                         artistBio:(NSString *)aBio
+                        yearFormed:(int)aYear
+NS_DESIGNATED_INITIALIZER;
+
 @end
 
 NS_ASSUME_NONNULL_END

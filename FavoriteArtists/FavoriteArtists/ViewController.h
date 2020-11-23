b7c813f23fb2b0d8761ba7485e5a3a111ddea779
Craig Belinfante <craigbasketball@aol.com>
2020-11-22 18:24:45 -0800

    craigbel setup project

---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by Craig Belinfante on 11/22/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Craig Belinfante on 11/22/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

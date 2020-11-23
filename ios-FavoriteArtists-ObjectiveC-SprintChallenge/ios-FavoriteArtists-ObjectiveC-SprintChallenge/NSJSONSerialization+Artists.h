9faa0175dc9995394b86831bdcb6435454018d8f
brys <brys1saclaus2@gmail.com>
2020-11-22 23:39:34 -0800

    added NSJSONSerialization category file

---
+++ ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/NSJSONSerialization+Artists.h
@@ -0,0 +1,16 @@
+//
+//  NSJSONSerialization+Artists.h
+//  ios-FavoriteArtists-ObjectiveC-SprintChallenge
+//
+//  Created by BrysonSaclausa on 11/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSJSONSerialization (Artists)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios-FavoriteArtists-ObjectiveC-SprintChallenge/ios-FavoriteArtists-ObjectiveC-SprintChallenge/NSJSONSerialization+Artists.m
@@ -0,0 +1,12 @@
+//
+//  NSJSONSerialization+Artists.m
+//  ios-FavoriteArtists-ObjectiveC-SprintChallenge
+//
+//  Created by BrysonSaclausa on 11/22/20.
+//
+
+#import "NSJSONSerialization+Artists.h"
+
+@implementation NSJSONSerialization (Artists)
+
+@end

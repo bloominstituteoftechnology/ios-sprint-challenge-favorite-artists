2659997e9cc9561f07892b8d528f524e3adcf053
Gladymir <gaethano815@gmail.com>
2020-11-29 13:55:09 -0700

    almost done

---
+++ FavoriteArtists/FavoriteArtists/ViewControllers/MyArtistTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MyArtistTableViewController.h
+//  FavoriteArtists
+//
+//  Created by Gladymir Philippe on 11/22/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyArtistTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

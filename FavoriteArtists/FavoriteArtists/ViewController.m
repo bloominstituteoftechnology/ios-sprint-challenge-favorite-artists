9135737c1527a3920d58f9c29c87a0b32a5d8fcb
Bohdan <tech.bohdan@gmail.com>
2020-11-30 20:30:39 -0600

    Created a project

---
+++ FavoriteArtists/FavoriteArtists/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Bohdan Tkachenko on 11/30/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

5e69b504878fdc8c9944b2910b04a0afafefb0c4
Kenneth Jones <kennethjones1991@outlook.com>
2020-12-03 17:11:20 -0700

    Created initial project

---
+++ FavoriteArtists/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by Kenneth Jones on 12/3/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  FavoriteArtists
+//
+//  Created by Kenneth Jones on 12/3/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

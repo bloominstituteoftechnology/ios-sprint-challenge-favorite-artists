1626b714a7747a6c5a05139547ce0be6b075a64e
johnmccants002 <johnmccants002@gmail.com>
2021-02-18 19:24:34 -0800

    Set up storyboard and constraints

---
+++ FavoriteArtists/FavoriteArtists/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FavoriteArtists
+//
+//  Created by John McCants on 2/18/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ FavoriteArtists/FavoriteArtists/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FavoriteArtists
+//
+//  Created by John McCants on 2/18/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
